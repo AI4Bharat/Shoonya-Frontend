d14e20293ca9aefe1e40a067118a38906d3894b5
payyup <ayushpanwar25@gmail.com>
2022-06-14 09:49:39 +0530

    Add date picker

---
+++ src/redux/actions/api/ProjectDetails/GetProjectReport.js
@@ -0,0 +1,49 @@
+/**
+ * Login API
+ */
+import API from "../../../api";
+import ENDPOINTS from "../../../../config/apiendpoint";
+import constants from "../../../constants";
+ 
+export default class GetProjectReportAPI extends API {
+   constructor(projectId, startDate, endDate, timeout = 2000) {
+     super("POST", timeout, false);
+     this.startDate = startDate;
+     this.endDate = endDate;
+     this.type = constants.GET_PROJECT_REPORT;
+     this.endpoint = `${super.apiEndPointAuto()}${ENDPOINTS.getProjects}${projectId}/get_analytics`;
+   }
+ 
+   processResponse(res) {
+     super.processResponse(res);
+     if (res) {
+         this.projectReport = res;
+     }
+ }
+ 
+   apiEndPoint() {
+     return this.endpoint;
+   }
+
+   getBody() {
+    return {
+      from_date: this.startDate,
+      to_date: this.endDate,
+    };
+  }
+ 
+   getHeaders() {
+     this.headers = {
+       headers: {
+         "Content-Type": "application/json",
+         "Authorization":`JWT ${localStorage.getItem('shoonya_access_token')}`
+       },
+     };
+     return this.headers;
+   }
+ 
+   getPayload() {
+     return this.projectReport;
+   }
+ }
+ 
--- src/ui/pages/component/Project/ReportsTable.jsx
+++ src/ui/pages/component/Project/ReportsTable.jsx
@@ -3,90 +3,198 @@
 import * as React from 'react';
 import MUIDataTable from "mui-datatables";
 import CustomButton from '../common/Button';
-import { Typography } from '@mui/material';
+import { Typography, TextField, Box, Stack, Button } from '@mui/material';
+import { DateRangePicker, LocalizationProvider   } from '@mui/x-date-pickers-pro';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { format, addDays, addWeeks, startOfWeek, startOfMonth, lastDayOfWeek } from 'date-fns';
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import GetProjectReportAPI from "../../../../redux/actions/api/ProjectDetails/GetProjectReport";
+import APITransport from '../../../../redux/actions/apitransport/apitransport';
 
-
-const columns = [
-    {
-        name: "Username",
-        label: "Username",
-        options: {
-            filter: false,
-            sort: false,
-            align : "center"
-        }
-    },
-    {
-        name: "Mail",
-        label: "Mail",
-        options: {
-            filter: false,
-            sort: false,
-        }
-    },
-    {
-        name: "Total Annoted Tasks",
-        label: "Total Annoted Tasks",
-        options: {
-            filter: false,
-            sort: false,
-        }
-    },
-    {
-        name: "Avg Lead Time",
-        label: "Avg Lead Time",
-        options: {
-            filter: false,
-            sort: false,
-        }
-    },
-    {
-        name: "Total Assigned Time",
-        label: "Total Assigned Time",
-        options: {
-            filter: false,
-            sort: false,
-        }
-    },
-    {
-        name: "Skipped Time",
-        label: "Skipped Time",
-        options: {
-            filter: false,
-            sort: false,
-        }
-    },
-    {
-        name: "Total Pending Time",
-        label: "Total Pending Time",
-        options: {
-            filter: false,
-            sort: false,
-        }
-    }
-];
-const data = [
-    ["Shoonya User", "user123@tarento.com", 0, 5, 4, 7, 8, 9]
-];
-
-const options = {
-    filterType: 'checkbox',
-    selectableRows: "none",
-    download : false,
-    filter : false,
-    print : false
-};
 
 const ReportsTable = () => {
+    const [startDate, setStartDate] = React.useState(format(startOfMonth(Date.now()), 'yyyy-MM-dd'));
+    const [endDate, setEndDate] = React.useState(format(Date.now(), 'yyyy-MM-dd'));
+    const [selectRange, setSelectRange] = React.useState("This Month");
+    const [rangeValue, setRangeValue] = React.useState([startOfMonth(Date.now()), Date.now()]);
+    const [reportData, setReportData] = React.useState([[]]);
+    const [showPicker, setShowPicker] = React.useState(false);
+    const [showTable, setShowTable] = React.useState(true);
+
+    const { id } = useParams();
+    const dispatch = useDispatch();
+    const ProjectReport = useSelector(state => state.getProjectReport.data);
+
+    const getProjectReport = () => {
+        const projectObj = new GetProjectReportAPI(id, startDate, endDate);
+        dispatch(APITransport(projectObj));
+    }
+
+    const columns = [
+        {
+            name: "username",
+            label: "Username",
+            options: {
+                filter: false,
+                sort: false,
+                align : "center"
+            }
+        },
+        {
+            name: "mail",
+            label: "Mail",
+            options: {
+                filter: false,
+                sort: false,
+            }
+        },
+        {
+            name: "total_annoted_tasks",
+            label: "Total Annoted Tasks",
+            options: {
+                filter: false,
+                sort: false,
+            }
+        },
+        {
+            name: "avg_lead_time",
+            label: "Avg Lead Time",
+            options: {
+                filter: false,
+                sort: false,
+            }
+        },
+        {
+            name: "total_assigned_tasks",
+            label: "Total Assigned Time",
+            options: {
+                filter: false,
+                sort: false,
+            }
+        },
+        {
+            name: "skipped_tasks",
+            label: "Skipped Time",
+            options: {
+                filter: false,
+                sort: false,
+            }
+        },
+        {
+            name: "total_pending_tasks",
+            label: "Total Pending Time",
+            options: {
+                filter: false,
+                sort: false,
+            }
+        }
+    ];
+
+    const options = {
+        filterType: 'checkbox',
+        selectableRows: "none",
+        download : false,
+        filter : false,
+        print : false
+    };
+
+    const handleOptionChange = (e) => {
+        setSelectRange(e.target.value);
+        if (e.target.value === "Custom Range") {
+            setStartDate(format(startOfMonth(Date.now()), 'yyyy-MM-dd'));
+            setEndDate(format(Date.now(), 'yyyy-MM-dd'));
+            setShowPicker(true);
+        } else setShowPicker(false);
+        if (e.target.value === "Today") {
+            setStartDate(format(Date.now(), 'yyyy-MM-dd'));
+            setEndDate(format(Date.now(), 'yyyy-MM-dd'));
+        }
+        if (e.target.value === "Yesterday") {
+            setStartDate(format(addDays(Date.now(), -1), 'yyyy-MM-dd'));
+            setEndDate(format(addDays(Date.now(), -1), 'yyyy-MM-dd'));
+        }
+        if (e.target.value === "This Week") {
+            setStartDate(format(startOfWeek(Date.now()), 'yyyy-MM-dd'));
+            setEndDate(format(Date.now(), 'yyyy-MM-dd'));
+        }
+        if (e.target.value === "Last Week") {
+            setStartDate(format(startOfWeek(addWeeks(Date.now(), -1)), 'yyyy-MM-dd'));
+            setEndDate(format(lastDayOfWeek(addWeeks(Date.now(), -1)), 'yyyy-MM-dd'));
+        }
+        if (e.target.value === "This Month") {
+            setStartDate(format(startOfMonth(Date.now()), 'yyyy-MM-dd'));
+            setEndDate(format(Date.now(), 'yyyy-MM-dd'));
+        }
+    };
+
+    const handleRangeChange = (dates) => {
+        setRangeValue(dates);
+        const [start, end] = dates;
+        setStartDate(format(start, 'yyyy-MM-dd'));
+        setEndDate(format(end, 'yyyy-MM-dd'));
+    };
+
+    const handleDateSubmit = () => {
+        getProjectReport();
+        setReportData(ProjectReport);
+        if(ProjectReport.length > 0) setShowTable(true);
+    }
+
     return (
         <React.Fragment>
-            <MUIDataTable
-                title={""}
-                data={data}
-                columns={columns}
-                options={options}
-                // filter={false}
-            />
+            <Stack direction="row" spacing={2} sx={{ marginBottom: "12px", alignItems: "center" }}>
+                <Box sx={{ width: "200px" }}>
+                    <FormControl variant="standard" fullWidth>
+                        <InputLabel id="date-range-select-label">Date Range</InputLabel>
+                            <Select
+                                labelId="date-range-select-label"
+                                id="date-range-select"
+                                value={selectRange}
+                                defaultValue={"This Month"}
+                                label="Date Range"
+                                onChange={handleOptionChange}
+                            >
+                                <MenuItem value={"Today"}>Today</MenuItem>
+                                <MenuItem value={"Yesterday"}>Yesterday</MenuItem>
+                                <MenuItem value={"This Week"}>This Week</MenuItem>
+                                <MenuItem value={"Last Week"}>Last Week</MenuItem>
+                                <MenuItem value={"This Month"}>This Month</MenuItem>
+                                <MenuItem value={"Custom Range"}>Custom Range</MenuItem>
+                            </Select>
+                    </FormControl>
+                </Box>
+                {showPicker &&
+                    <LocalizationProvider
+                        dateAdapter={AdapterDateFns}
+                        localeText={{ start: 'Start Date', end: 'End Date' }}
+                    >
+                        <DateRangePicker
+                            value={rangeValue}
+                            onChange={handleRangeChange}
+                            renderInput={(startProps, endProps) => (
+                                <React.Fragment>
+                                    <TextField {...startProps} />
+                                        <Box sx={{ mx: 2 }}> to </Box>
+                                    <TextField {...endProps} />
+                                </React.Fragment>
+                            )}
+                        />
+                    </LocalizationProvider>}
+                <Button variant="contained" onClick={handleDateSubmit}>Submit</Button>
+            </Stack>
+            {showTable &&
+                <MUIDataTable
+                    title={""}
+                    data={reportData}
+                    columns={columns}
+                    options={options}
+                    // filter={false}
+                />}
         </React.Fragment>
     )
 }
