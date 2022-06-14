248aaec5afcf0938a4a4f6790377e6e4304df911
Mrigank Shekhar Shringi <mrigank.shringi@tarento.com>
2022-06-14 14:48:52 +0530

    cloned workspace folder

---
+++ src/ui/pages/container/Workspace copy/WorkSpaceList.jsx
@@ -0,0 +1,37 @@
+import { Box, Divider, Grid, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import Header from "../../component/common/Header";
+import ProjectCard from "../../component/common/ProjectCard";
+import WorkspaceTable from "../../component/common/WorkspaceTable";
+import DatasetStyle from "../../../styles/Dataset";
+import GetProjectsAPI from "../../../../redux/actions/api/Dashboard/GetProjects";
+import {useDispatch,useSelector} from 'react-redux';
+import APITransport from '../../../../redux/actions/apitransport/apitransport';
+import GetWorkspacesAPI from "../../../../redux/actions/api/Dashboard/GetWorkspaces";
+
+export default function WorkSpaces(props) {
+ 
+    const classes = DatasetStyle();
+    const dispatch = useDispatch();
+    const workspaceData = useSelector(state=>state.getWorkspaces.data);
+
+    const getDashboardWorkspaceData = ()=>{
+    const workspaceObj = new GetWorkspacesAPI(1);
+    dispatch(APITransport(workspaceObj));
+  }
+  
+  useEffect(()=>{
+    getDashboardWorkspaceData();
+  },[]);
+    
+
+  return (
+    <React.Fragment>
+    {/* <Header /> */}
+    <Box sx={{ width: window.innerWidth*0.7, margin : "0 auto", pb : 5 }}>
+        {/* <Typography variant="h5" sx={{mt : 2, mb : 2}}>Visit Workspaces</Typography> */}
+        <WorkspaceTable workspaceData={workspaceData} />
+    </Box>
+</React.Fragment>
+  )
+}
