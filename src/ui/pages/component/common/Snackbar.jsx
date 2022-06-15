f876a2dd531e420e7106ab23ed743a058268502b
AlpanaMajhi <alpanamajhi001@gmail.com>
2022-06-15 18:24:09 +0530

    Common Snackbar added

---
+++ src/ui/pages/component/common/Snackbar.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
+import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles';
+import { createTheme } from '@mui/material/styles';
+
+const CustomizedSnackbars = (props) => {
+
+    const getMuiTheme = () => createTheme({
+        components: {
+            MuiSnackbar: {
+                styleOverrides:{
+                    anchorOriginTopRight: {
+                        marginTop: "50px"
+                    }
+                }
+            }
+        }
+    });
+
+    return (
+        <MuiThemeProvider theme={getMuiTheme()}>
+                <Snackbar
+                    autoHideDuration={props.hide? props.hide : 20000}
+                    open={props.open}
+                    onClose={props.handleClose}
+                    anchorOrigin={props.anchorOrigin}
+                >
+                    <Alert elevation={3}
+                        variant="filled"
+                        onClose={props.handleClose}
+                        severity={props.variant}>
+                        {props.message}
+                    </Alert>
+                </Snackbar>
+        </MuiThemeProvider>
+    );
+}
+
+export default CustomizedSnackbars;
