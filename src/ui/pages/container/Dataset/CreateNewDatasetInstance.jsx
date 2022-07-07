bb8f8dcd94b44d50535039edda39960180cd1b30
ganavikumarswamy <ganavi.kumarswamy@tarento.com>
2022-07-07 16:07:39 +0530

    fixed the issue in Create Workspace

--- src/ui/pages/container/Dataset/CreateNewDatasetInstance.jsx
+++ src/ui/pages/container/Dataset/CreateNewDatasetInstance.jsx
@@ -1,70 +1,84 @@
 import {
-    Box,
-    Card,
-    Grid,
-    Tab,
-    Tabs,
-    ThemeProvider,
-    Typography,
-  } from "@mui/material";
-  import React, { useEffect, useState } from "react";
-  import Header from "../../component/common/Header";
-  import themeDefault from "../../../theme/theme";
-  import { Link, useNavigate, useParams } from "react-router-dom";
-  import Button from "../../component/common/Button";
-  import OutlinedTextField from "../../component/common/OutlinedTextField";
-  import DatasetStyle from "../../../styles/Dataset";
-  import { useDispatch, useSelector } from "react-redux";
-  import APITransport from "../../../../redux/actions/apitransport/apitransport";
-  import CreateNewDatasetInstanceAPI from "../../../../redux/actions/api/Dataset/CreateNewDatasetInstance";
-  import GetDatasetTypeAPI from "../../../../redux/actions/api/Dataset/GetDatasetType"
-  import MenuItems from "../../component/common/MenuItems"
-  
-  const CollectionProject = (props) => {
-    const { id } = useParams();
-     const navigate = useNavigate();
-    const classes = DatasetStyle();
-    const dispatch = useDispatch();
-   const [instance_Name, setInstance_Name] = useState("")
-   const [parent_Instance_Id, setParent_Instance_Id] = useState("")
-   const [instance_Description, setInstance_Description] = useState("")
-   const [dataset_Type, setDataset_Type] = useState("")
-   const [organisation_Id, setOrganisation_Id] = useState("")
-   const [users, setUsers] = useState("")
+  Box,
+  Card,
+  Grid,
+  Tab,
+  Tabs,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import Header from "../../component/common/Header";
+import themeDefault from "../../../theme/theme";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import Button from "../../component/common/Button";
+import OutlinedTextField from "../../component/common/OutlinedTextField";
+import DatasetStyle from "../../../styles/Dataset";
+import { useDispatch, useSelector } from "react-redux";
+import APITransport from "../../../../redux/actions/apitransport/apitransport";
+import CreateNewDatasetInstanceAPI from "../../../../redux/actions/api/Dataset/CreateNewDatasetInstance";
+import GetDatasetTypeAPI from "../../../../redux/actions/api/Dataset/GetDatasetType"
+import MenuItems from "../../component/common/MenuItems"
 
-     
-    const handleCreate =()=>{
-      const CreateDatasetInstance={
-        instance_name:instance_Name,
-        parent_instance_id:parent_Instance_Id,
-        instance_description:instance_Description,
-        dataset_type:dataset_Type,
-        organisation_id:organisation_Id,
-        users:users,
-      }
-        const projectObj = new CreateNewDatasetInstanceAPI(CreateDatasetInstance);
-        dispatch(APITransport(projectObj)); 
+const CollectionProject = (props) => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const classes = DatasetStyle();
+  const dispatch = useDispatch();
+  const [instance_Name, setInstance_Name] = useState("")
+  const [parent_Instance_Id, setParent_Instance_Id] = useState("")
+  const [instance_Description, setInstance_Description] = useState("")
+  const [dataset_Type, setDataset_Type] = useState("")
+  const [organisation_Id, setOrganisation_Id] = useState("")
+  const [users, setUsers] = useState("")
+  const [datasettype, setDatasettype] = useState("");
+  const [type, setType] = useState([]);
+
+console.log(datasettype,"datasettype",setDatasettype,"setDatasettype")
+
+  const handleCreate = () => {
+    const CreateDatasetInstance = {
+      instance_name: instance_Name,
+      parent_instance_id: parent_Instance_Id,
+      instance_description: instance_Description,
+      datasettype: datasettype,
+      organisation_id: organisation_Id,
+      users: users,
     }
+    const projectObj = new CreateNewDatasetInstanceAPI(CreateDatasetInstance);
+    dispatch(APITransport(projectObj));
+  }
 
-    const datasetType = useSelector(state => state.GetDatasetType.data);
+  const datasetType = useSelector(state => state.GetDatasetType.data);
 
-    const getProjectDetails = () => {
-        const projectObj = new GetDatasetTypeAPI(id);
-        dispatch(APITransport(projectObj));
+  const getProjectDetails = () => {
+    const projectObj = new GetDatasetTypeAPI(id);
+    dispatch(APITransport(projectObj));
+  }
+
+  useEffect(() => {
+    getProjectDetails();
+
+  }, []);
+  console.log(datasetType, "datasetType")
+
+
+  useEffect(() => {
+    if (datasetType && datasetType.length > 0) {
+      let temp = [];
+      datasetType.forEach((element) => {
+        temp.push({
+          name: element,
+
+        });
+      });
+      setType(temp);
     }
-
-    useEffect(() => {
-        getProjectDetails();
-
-    }, []);
-    console.log(datasetType, "datasetType")
-    
-   
-
-    return (
-      <ThemeProvider theme={themeDefault}>
-        {/* <Header /> */}
-        {/* <Grid
+  }, [datasetType]);
+  return (
+    <ThemeProvider theme={themeDefault}>
+      {/* <Header /> */}
+      {/* <Grid
                   container
                   direction='row'
                   justifyContent='left'
@@ -80,8 +94,8 @@ import {
                       lg={5}
                       xl={5}
                   > */}
-                  
-        <Grid container direction="row"  >
+
+      <Grid container direction="row"  >
         <Card className={classes.workspaceCard}>
           <Grid item xs={2} sm={2} md={2} lg={2} xl={2}></Grid>
           <Grid item xs={8} sm={8} md={8} lg={8} xl={8} sx={{ pb: "6rem" }}>
@@ -90,155 +104,154 @@ import {
                 Create a Project
               </Typography>
             </Grid>
-          <Grid
-            className={classes.projectsettingGrid}
-            xs={12}
-            sm={12}
-            md={12}
-            lg={12}
-            xl={12}
-          >
-            <Typography gutterBottom component="div">
-            Instance_Name*:
-            </Typography>
-          </Grid>
-          <Grid item xs={12} md={12} lg={12} xl={12} sm={12}>
-            <OutlinedTextField
-              fullWidth
-              value={instance_Name}
-              onChange={(e) => setInstance_Name(e.target.value)}
-            />
-          </Grid>
-          <Grid
-            className={classes.projectsettingGrid}
-            xs={12}
-            sm={12}
-            md={12}
-            lg={12}
-            xl={12}
-          >
-            <Typography gutterBottom component="div">
-          Parent_Instance_Id:
-            </Typography>
-          </Grid>
-          <Grid item xs={12} md={12} lg={12} xl={12} sm={12}>
-            <OutlinedTextField
-              fullWidth
-              value={parent_Instance_Id}
-              onChange={(e) => setParent_Instance_Id(e.target.value)}
-            />
-          </Grid>
-          <Grid
-            className={classes.projectsettingGrid}
-            xs={12}
-            sm={12}
-            md={12}
-            lg={12}
-            xl={12}
-          >
-            <Typography gutterBottom component="div">
-            Instance_Description:
-            </Typography>
-          </Grid>
-          <Grid item xs={12} md={12} lg={12} xl={12} sm={12}>
-            <OutlinedTextField
-              fullWidth
-              value={instance_Description}
-              onChange={(e) => setInstance_Description(e.target.value)}
-            />
-          </Grid>
-          <Grid
-            className={classes.projectsettingGrid}
-            xs={12}
-            sm={12}
-            md={12}
-            lg={12}
-            xl={12}
-          >
-            <Typography gutterBottom component="div">
-            Dataset_Type*:
-            </Typography>
-          </Grid>
-          <Grid item xs={12} md={12} lg={12} xl={12} sm={12}>
-            <OutlinedTextField
-              fullWidth
-              value={dataset_Type}
-              onChange={(e) => setDataset_Type(e.target.value)}
-            />
-          </Grid>
-          <Grid
-            className={classes.projectsettingGrid}
-            xs={12}
-            sm={12}
-            md={12}
-            lg={12}
-            xl={12}
-          >
-            <Typography gutterBottom component="div">
-            Organisation_Id:
-            </Typography>
-          </Grid>
-          <Grid item xs={12} md={12} lg={12} xl={12} sm={12}>
-            <OutlinedTextField
-              fullWidth
-              value={organisation_Id}
-              onChange={(e) => setOrganisation_Id(e.target.value)}
-            />
-          </Grid>
-          <Grid
-            className={classes.projectsettingGrid}
-            xs={12}
-            sm={12}
-            md={12}
-            lg={12}
-            xl={12}
-          >
-            <Typography gutterBottom component="div">
-            User*:
-            </Typography>
-          </Grid>
-          <Grid item xs={12} md={12} lg={12} xl={12} sm={12}>
-            <OutlinedTextField
-              fullWidth
-              value={users}
-              onChange={(e) => setUsers(e.target.value)}
-            />
-          </Grid>
-          <Grid
-            className={classes.projectsettingGrid}
-            style={{}}
-            item
-            xs={12}
-            md={12}
-            lg={12}
-            xl={12}
-            sm={12}
-            sx={{mt:3}}
-          >
-           
+            <Grid
+              className={classes.projectsettingGrid}
+              xs={12}
+              sm={12}
+              md={12}
+              lg={12}
+              xl={12}
+            >
+              <Typography gutterBottom component="div">
+                Instance_Name*:
+              </Typography>
+            </Grid>
+            <Grid item xs={12} md={12} lg={12} xl={12} sm={12}>
+              <OutlinedTextField
+                fullWidth
+                value={instance_Name}
+                onChange={(e) => setInstance_Name(e.target.value)}
+              />
+            </Grid>
+            <Grid
+              className={classes.projectsettingGrid}
+              xs={12}
+              sm={12}
+              md={12}
+              lg={12}
+              xl={12}
+            >
+              <Typography gutterBottom component="div">
+                Parent_Instance_Id:
+              </Typography>
+            </Grid>
+            <Grid item xs={12} md={12} lg={12} xl={12} sm={12}>
+              <OutlinedTextField
+                fullWidth
+                value={parent_Instance_Id}
+                onChange={(e) => setParent_Instance_Id(e.target.value)}
+              />
+            </Grid>
+            <Grid
+              className={classes.projectsettingGrid}
+              xs={12}
+              sm={12}
+              md={12}
+              lg={12}
+              xl={12}
+            >
+              <Typography gutterBottom component="div">
+                Instance_Description:
+              </Typography>
+            </Grid>
+            <Grid item xs={12} md={12} lg={12} xl={12} sm={12}>
+              <OutlinedTextField
+                fullWidth
+                value={instance_Description}
+                onChange={(e) => setInstance_Description(e.target.value)}
+              />
+            </Grid>
+            <Grid
+              className={classes.projectsettingGrid}
+              xs={12}
+              sm={12}
+              md={12}
+              lg={12}
+              xl={12}
+            >
+              <Typography gutterBottom component="div">
+                Dataset_Type*:
+              </Typography>
+            </Grid>
+            <Grid item xs={12} md={12} lg={12} xl={12} sm={12}>
+              <MenuItems
+                menuOptions={type}
+                handleChange={(value) => setDatasettype(value)}
+                value={datasettype}
+              />
+            </Grid>
+            <Grid
+              className={classes.projectsettingGrid}
+              xs={12}
+              sm={12}
+              md={12}
+              lg={12}
+              xl={12}
+            >
+              <Typography gutterBottom component="div">
+                Organisation_Id:
+              </Typography>
+            </Grid>
+            <Grid item xs={12} md={12} lg={12} xl={12} sm={12}>
+              <OutlinedTextField
+                fullWidth
+                value={organisation_Id}
+                onChange={(e) => setOrganisation_Id(e.target.value)}
+              />
+            </Grid>
+            <Grid
+              className={classes.projectsettingGrid}
+              xs={12}
+              sm={12}
+              md={12}
+              lg={12}
+              xl={12}
+            >
+              <Typography gutterBottom component="div">
+                User*:
+              </Typography>
+            </Grid>
+            <Grid item xs={12} md={12} lg={12} xl={12} sm={12}>
+              <OutlinedTextField
+                fullWidth
+                value={users}
+                onChange={(e) => setUsers(e.target.value)}
+              />
+            </Grid>
+            <Grid
+              className={classes.projectsettingGrid}
+              style={{}}
+              item
+              xs={12}
+              md={12}
+              lg={12}
+              xl={12}
+              sm={12}
+              sx={{ mt: 3 }}
+            >
+
               <Button
                 style={{ margin: "0px 20px 0px 0px" }}
                 label={"Create Project"}
-               onClick={handleCreate}
-               
+                onClick={handleCreate}
+
               />
-           
-            <Button
-              label={"Cancel"}
-              onClick={() => navigate(`/datasets/`)}
-            />
+
+              <Button
+                label={"Cancel"}
+                onClick={() => navigate(`/datasets/`)}
+              />
+            </Grid>
+
+
           </Grid>
-          
-     
-          </Grid>
-          </Card>
-        </Grid>
-      
-        {/* </Grid>
+        </Card>
+      </Grid>
+
+      {/* </Grid>
               </Grid> */}
-      </ThemeProvider>
-    );
-  };
-  
-  export default CollectionProject;
-  
+    </ThemeProvider>
+  );
+};
+
+export default CollectionProject;
