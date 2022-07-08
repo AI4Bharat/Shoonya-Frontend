271ea0b0f27df64da3b1e00ffe4cadec290c3940
AlpanaMajhi <alpanamajhi001@gmail.com>
2022-07-08 15:15:17 +0530

    notes

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,7 @@ const configs = {
       ? process.env.REACT_APP_APIGW_BASE_URL
         //  : "https://backend.shoonya.ai4bharat.org",
         // :"http://backend.stage.shoonya.ai4bharat.org",
-       :"http://20.51.211.111:8000"
+      :"http://20.51.211.111:8000"
   };
   
   export default configs;
