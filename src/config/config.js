c1438177816f4ad08e10cc6125290d532e3a5e8e
AlpanaMajhi <alpanamajhi001@gmail.com>
2022-07-08 10:48:25 +0530

    stage backend

--- src/config/config.js
+++ src/config/config.js
@@ -3,8 +3,10 @@ const configs = {
     BASE_URL_AUTO: process.env.REACT_APP_APIGW_BASE_URL
       ? process.env.REACT_APP_APIGW_BASE_URL
         //  : "https://backend.shoonya.ai4bharat.org",
-        // :"http://backend.stage.shoonya.ai4bharat.org",
-       :"http://20.51.211.111:8000"
+         :"http://backend.stage.shoonya.ai4bharat.org",
+      //  :"http://20.51.211.111:8000"
   };
   
   export default configs;
+
+  
