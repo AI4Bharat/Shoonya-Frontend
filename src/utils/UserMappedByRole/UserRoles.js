78c9236e5cdbb744ab7edc49ef0613e2040a662a
aditya-mitra <55396651+aditya-mitra@users.noreply.github.com>
2022-07-15 12:25:32 +0530

    add reviewer and admin

--- src/utils/UserMappedByRole/UserRoles.js
+++ src/utils/UserMappedByRole/UserRoles.js
@@ -2,6 +2,8 @@ export const UserRoles = {
 	ANNOTATOR: 1,
 	WORKSPACE_MANAGER: 2,
 	ORGANIZATION_OWNER: 3,
+	REVIEWER: 4,
+	ADMIN: 5,
 };
 
 export function checkUserRole(role) {
@@ -9,6 +11,8 @@ export function checkUserRole(role) {
 		isAnnotator: (role) => role === UserRoles.ANNOTATOR,
 		isWorkspaceManager: (role) => role === UserRoles.WORKSPACE_MANAGER,
 		isOrganizationOwner: (role) => role === UserRoles.ORGANIZATION_OWNER,
+		isReviewer: (role) => role === UserRoles.REVIEWER,
+		isAdmin: (role) => role === UserRoles.ADMIN,
 		isAnnotatorOrOrganizationOwner: (role) =>
 			role === UserRoles.ANNOTATOR ||
 			role === UserRoles.ORGANIZATION_OWNER,
