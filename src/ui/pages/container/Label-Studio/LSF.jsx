1739eba0404c30373faf49c8afc222edd20d2ea9
Mrigank Shekhar Shringi <mrigank.shringi@tarento.com>
2022-07-29 16:52:38 +0530

    moved LSF sidebar to bottom for some more space

--- src/ui/pages/container/Label-Studio/LSF.jsx
+++ src/ui/pages/container/Label-Studio/LSF.jsx
@@ -29,12 +29,12 @@ import { translate } from '../../../../config/localisation';
 
 //used just in postAnnotation to support draft status update.
 
-const LabelStudioWrapper = ({annotationNotesRef, loader, showLoader, hideLoader, resetNotes}) => {
+const LabelStudioWrapper = ({ annotationNotesRef, loader, showLoader, hideLoader, resetNotes }) => {
   // we need a reference to a DOM node here so LSF knows where to render
   const rootRef = useRef();
   const dispatch = useDispatch();
   const ProjectDetails = useSelector(state => state.getProjectDetails.data);
-  const task_status = useRef(ProjectDetails.enable_task_reviews ? "labeled": "accepted");
+  const task_status = useRef(ProjectDetails.enable_task_reviews ? "labeled" : "accepted");
   // this reference will be populated when LSF initialized and can be used somewhere else
   const lsfRef = useRef();
   const navigate = useNavigate();
@@ -46,11 +46,31 @@ const LabelStudioWrapper = ({annotationNotesRef, loader, showLoader, hideLoader,
   });
   const [taskData, setTaskData] = useState(undefined);
   const { projectId, taskId } = useParams();
-  const userData = useSelector(state=>state.fetchLoggedInUserData.data)
+  const userData = useSelector(state => state.fetchLoggedInUserData.data)
   let loaded = useRef();
 
   console.log("projectId, taskId", projectId, taskId);
   // debugger
+
+  useEffect(() => {
+    localStorage.setItem("labelStudio:settings", JSON.stringify({
+      bottomSidePanel: true,
+      continuousLabeling: false,
+      enableAutoSave: false,
+      enableHotkeys: true,
+      enableLabelTooltips: true,
+      enablePanelHotkeys: true,
+      enableTooltips: false,
+      fullscreen: false,
+      imageFullSize: false,
+      selectAfterCreate: false,
+      showAnnotationsPanel: true,
+      showLabels: false,
+      showLineNumbers: false,
+      showPredictionsPanel: true,
+      sidePanelMode: "SIDEPANEL_MODE_REGIONS"
+    }))
+  }, [])
 
   const tasksComplete = (id) => {
     if (id) {
@@ -160,7 +180,7 @@ const LabelStudioWrapper = ({annotationNotesRef, loader, showLoader, hideLoader,
         },
 
         onLabelStudioLoad: function (ls) {
-          task_status.current = ProjectDetails.enable_task_reviews ? "labeled": "accepted";
+          task_status.current = ProjectDetails.enable_task_reviews ? "labeled" : "accepted";
           console.log("task_status", task_status.current, "test", ProjectDetails);
           if (annotations.length === 0) {
             var c = ls.annotationStore.addAnnotation({
@@ -195,15 +215,15 @@ const LabelStudioWrapper = ({annotationNotesRef, loader, showLoader, hideLoader,
             })
           }
           else
-          setSnackbarInfo({
-            open: true,
-            message: "Task is frozen",
-            variant: "error",
-          });
+            setSnackbarInfo({
+              open: true,
+              message: "Task is frozen",
+              variant: "error",
+            });
         },
 
         onSkipTask: function () {
-        //   message.warning('Notes will not be saved for skipped tasks!');
+          //   message.warning('Notes will not be saved for skipped tasks!');
           showLoader();
           updateTask(taskData.id).then(() => {
             getNextProject(projectId, taskData.id).then((res) => {
@@ -232,26 +252,26 @@ const LabelStudioWrapper = ({annotationNotesRef, loader, showLoader, hideLoader,
                   annotations[i].lead_time,
                   task_status.current,
                   annotationNotesRef.current.value
-                  ).then(() => {
-                    if (localStorage.getItem("labelAll"))
-                      getNextProject(projectId, taskData.id).then((res) => {
-                        hideLoader();
-                        tasksComplete(res?.id || null);
-                      })
-                    else{
+                ).then(() => {
+                  if (localStorage.getItem("labelAll"))
+                    getNextProject(projectId, taskData.id).then((res) => {
                       hideLoader();
-                      window.location.reload();
-                    }
-                  });
+                      tasksComplete(res?.id || null);
+                    })
+                  else {
+                    hideLoader();
+                    window.location.reload();
+                  }
+                });
               }
             }
-          } 
+          }
           else
-          setSnackbarInfo({
-            open: true,
-            message: "Task is frozen",
-            variant: "error",
-          });
+            setSnackbarInfo({
+              open: true,
+              message: "Task is frozen",
+              variant: "error",
+            });
         },
 
         onDeleteAnnotation: function (ls, annotation) {
@@ -324,7 +344,7 @@ const LabelStudioWrapper = ({annotationNotesRef, loader, showLoader, hideLoader,
     getNextProject(projectId, taskId).then((res) => {
       hideLoader();
       // window.location.href = `/projects/${projectId}/task/${res.id}`;
-     tasksComplete(res?.id || null);
+      tasksComplete(res?.id || null);
     });
   }
 
@@ -346,46 +366,46 @@ const LabelStudioWrapper = ({annotationNotesRef, loader, showLoader, hideLoader,
   return (
     <div>
       {!loader && <div style={{ display: "flex", justifyContent: "space-between" }} className="lsf-controls">
-        <div/>
+        <div />
         <Grid container spacing={0}>
           <Grid item>
-          <Tooltip title="Save task for later">
-            <Button
-              value="Draft"
-              type="default"
-              onClick={handleDraftAnnotationClick}
-              style={{minWidth: "160px", border:"1px solid #e6e6e6", color: "#e80", pt: 3, pb: 3, borderBottom: "None"}}
-              className="lsf-button"
-            >
-              Draft
-            </Button>
-          </Tooltip>
-          </Grid>
-          <Grid item>
-          {localStorage.getItem("labelAll") !== "true" ? (
-            <Tooltip title="Go to next task">
+            <Tooltip title="Save task for later">
               <Button
-                value="Next"
+                value="Draft"
                 type="default"
-                onClick={onNextAnnotation}
-                style={{minWidth: "160px", border:"1px solid #e6e6e6", color: "#09f", pt: 3, pb: 3, borderBottom: "None",}}
+                onClick={handleDraftAnnotationClick}
+                style={{ minWidth: "160px", border: "1px solid #e6e6e6", color: "#e80", pt: 3, pb: 3, borderBottom: "None" }}
                 className="lsf-button"
               >
-                Next
+                Draft
               </Button>
             </Tooltip>
-          ) : (
-            <div style={{minWidth: "160px"}}/>
-          )}
+          </Grid>
+          <Grid item>
+            {localStorage.getItem("labelAll") !== "true" ? (
+              <Tooltip title="Go to next task">
+                <Button
+                  value="Next"
+                  type="default"
+                  onClick={onNextAnnotation}
+                  style={{ minWidth: "160px", border: "1px solid #e6e6e6", color: "#09f", pt: 3, pb: 3, borderBottom: "None", }}
+                  className="lsf-button"
+                >
+                  Next
+                </Button>
+              </Tooltip>
+            ) : (
+              <div style={{ minWidth: "160px" }} />
+            )}
           </Grid>
         </Grid>
       </div>}
       <Box
-        sx={{border : "1px solid rgb(224 224 224)"}}
+        sx={{ border: "1px solid rgb(224 224 224)" }}
       >
         <div className="label-studio-root" ref={rootRef}></div>
       </Box>
-      
+
       {loader}
       {renderSnackBar()}
     </div>
@@ -396,21 +416,21 @@ export default function LSF() {
   const [showNotes, setShowNotes] = useState(false);
   const annotationNotesRef = useRef(null);
   const reviewNotesRef = useRef(null);
-  const {taskId} = useParams()
+  const { taskId } = useParams()
   // const [notesValue, setNotesValue] = useState('');
   const { projectId } = useParams();
   const navigate = useNavigate();
   const [loader, showLoader, hideLoader] = useFullPageLoader();
-  
+
   const handleCollapseClick = () => {
     setShowNotes(!showNotes);
   }
 
-  useEffect(()=>{
-    fetchAnnotation(taskId).then((data)=>{
-      if(data && Array.isArray(data) && data.length > 0) {
-        annotationNotesRef.current.value = data[0].annotation_notes?? '';
-        reviewNotesRef.current.value = data[0].review_notes?? '';
+  useEffect(() => {
+    fetchAnnotation(taskId).then((data) => {
+      if (data && Array.isArray(data) && data.length > 0) {
+        annotationNotesRef.current.value = data[0].annotation_notes ?? '';
+        reviewNotesRef.current.value = data[0].review_notes ?? '';
       }
     })
   }, [taskId]);
@@ -421,16 +441,16 @@ export default function LSF() {
     reviewNotesRef.current.value = "";
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     resetNotes();
   }, [taskId]);
-  
+
   return (
     <div style={{ maxHeight: "100%", maxWidth: "100%", margin: "auto" }}>
       {!loader && <Button
         value="Back to Project"
         startIcon={<  ArrowBackIcon />}
-        variant="contained" 
+        variant="contained"
         color="primary"
         onClick={() => {
           localStorage.removeItem("labelAll");
@@ -443,28 +463,28 @@ export default function LSF() {
       </Button>}
       <Card
         sx={{
-            minHeight: 500,
-            padding: 5,
-            mt: 3,
-            pt: 3,
+          minHeight: 500,
+          padding: 5,
+          mt: 3,
+          pt: 3,
         }}
       >
-        {!loader && <Button 
+        {!loader && <Button
           endIcon={showNotes ? <ArrowRightIcon /> : <ArrowDropDownIcon />}
           variant="contained"
           color="primary"
           onClick={handleCollapseClick}
-          style={{marginBottom:'20px'}}
+          style={{ marginBottom: '20px' }}
         >
           Notes
         </Button>}
-        <div className={styles.collapse} style={{display: showNotes? "block" : "none",paddingBottom: "16px"}}>
-          <Alert severity="warning" showIcon style={{marginBottom: '1%'}}>
-              {translate("alert.notes")}
+        <div className={styles.collapse} style={{ display: showNotes ? "block" : "none", paddingBottom: "16px" }}>
+          <Alert severity="warning" showIcon style={{ marginBottom: '1%' }}>
+            {translate("alert.notes")}
           </Alert>
           <TextField
-            multiline 
-            placeholder="Place your remarks here ..." 
+            multiline
+            placeholder="Place your remarks here ..."
             label="Annotation Notes"
             // value={notesValue} 
             // onChange={event=>setNotesValue(event.target.value)} 
@@ -472,13 +492,13 @@ export default function LSF() {
             rows={2}
             maxRows={4}
             inputProps={{
-              style: {fontSize: "1rem",},
+              style: { fontSize: "1rem", },
             }}
-            style={{width: '99%'}}
+            style={{ width: '99%' }}
           />
           <TextField
-            multiline 
-            placeholder="Place your remarks here ..." 
+            multiline
+            placeholder="Place your remarks here ..."
             label="Review Notes"
             // value={notesValue} 
             // onChange={event=>setNotesValue(event.target.value)} 
@@ -486,13 +506,13 @@ export default function LSF() {
             rows={2}
             maxRows={4}
             inputProps={{
-              style: {fontSize: "1rem",},
+              style: { fontSize: "1rem", },
               readOnly: true,
             }}
-            style={{width: '99%', marginTop: '1%'}}
+            style={{ width: '99%', marginTop: '1%' }}
           />
         </div>
-        <LabelStudioWrapper resetNotes={()=>resetNotes()} annotationNotesRef={annotationNotesRef} loader={loader} showLoader={showLoader} hideLoader={hideLoader}/>
+        <LabelStudioWrapper resetNotes={() => resetNotes()} annotationNotesRef={annotationNotesRef} loader={loader} showLoader={showLoader} hideLoader={hideLoader} />
       </Card>
     </div>
   );
