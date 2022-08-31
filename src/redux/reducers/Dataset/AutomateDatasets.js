8bae044ce20daf5bd57cedeb59f5cbabed462835
payyup <ayushpanwar25@gmail.com>
2022-08-31 20:01:18 +0530

    Add dataset automation

---
+++ src/redux/reducers/Dataset/AutomateDatasets.js
@@ -0,0 +1,21 @@
+import constants from "../../constants";
+
+let initialState = {
+  data: {}
+}
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case constants.AUTOMATE_DATASETS:
+      return {
+        ...state,
+        data: action.payload
+      }
+
+    default:
+      return {
+        ...state
+      }
+  }
+};
+
+export default reducer;
