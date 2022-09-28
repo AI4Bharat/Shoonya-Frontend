440539597a8e0cf93bee1af74824c434ead9302d
ganavikumarswamy <ganavi.kumarswamy@tarento.com>
2022-09-28 18:01:04 +0530

    ui changes in my progress page

--- src/ui/pages/component/Tabs/MyProgress.jsx
+++ src/ui/pages/component/Tabs/MyProgress.jsx
@@ -9,7 +9,7 @@ import {
   InputLabel,
   FormControl,
   Card,
-  CircularProgress ,
+  CircularProgress,
   RadioGroup,
   FormControlLabel,
   Radio,
@@ -44,16 +44,17 @@ import { DateRangePicker, defaultStaticRanges } from "react-date-range";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 import { useParams } from "react-router-dom";
+import Spinner from "../../component/common/Spinner";
 
 const MyProgress = () => {
-  const {id } = useParams();
+  const { id } = useParams();
   const UserDetails = useSelector((state) => state.fetchLoggedInUserData.data);
   const [selectRange, setSelectRange] = useState([{
-      startDate: new Date(Date.parse(UserDetails?.date_joined, 'yyyy-MM-ddTHH:mm:ss.SSSZ')),
-      endDate: new Date(),
-      key: "selection"
+    startDate: new Date(Date.parse(UserDetails?.date_joined, 'yyyy-MM-ddTHH:mm:ss.SSSZ')),
+    endDate: new Date(),
+    key: "selection"
   }]);
-  console.log(UserDetails?.date_joined,"UserDetails?.date_joined")
+  console.log(UserDetails?.date_joined, "UserDetails?.date_joined")
   // const [rangeValue, setRangeValue] = useState([
   //   format(
   //     Date.parse(UserDetails?.date_joined, "yyyy-MM-ddTHH:mm:ss.SSSZ"),
@@ -73,12 +74,14 @@ const MyProgress = () => {
   const [submitted, setSubmitted] = useState(false);
   const [radiobutton, setRadiobutton] = useState("AnnotatationReports");
   const [workspaces, setWorkspaces] = useState([]);
+  const [totalsummary, setTotalsummary] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [selectedWorkspaces, setSelectedWorkspaces] = useState([]);
   const ProjectTypes = useSelector((state) => state.getProjectDomains.data);
   const Workspaces = useSelector((state) => state.getWorkspaces.data);
   const UserAnalytics = useSelector((state) => state.getUserAnalytics.data.project_summary);
-  const UserAnalyticstotalsummary= useSelector((state) => state.getUserAnalytics.data.total_summary);
-  //console.log(UserAnalyticstotalsummary ,"UserAnalytics111111")
+  const UserAnalyticstotalsummary = useSelector((state) => state.getUserAnalytics.data.total_summary);
+  const apiLoading = useSelector(state => state.apiStatus.loading);
   const dispatch = useDispatch();
 
   const classes = DatasetStyle();
@@ -88,11 +91,14 @@ const MyProgress = () => {
     dispatch(APITransport(typesObj));
     // const workspacesObj = new GetWorkspacesAPI(1, 9999);
     // dispatch(APITransport(workspacesObj));
-    
+
   }, []);
+  useEffect(() => {
+    setLoading(apiLoading);
+  }, [apiLoading])
 
 
- 
+
 
   // useEffect(() => {
   //   if (UserDetails && Workspaces?.results) {
@@ -154,7 +160,7 @@ const MyProgress = () => {
     const { selection } = ranges;
     if (selection.endDate > new Date()) selection.endDate = new Date();
     setSelectRange([selection]);
-    console.log(selection, "selection"); 
+    console.log(selection, "selection");
   };
 
   const handleProgressSubmit = () => {
@@ -165,17 +171,19 @@ const MyProgress = () => {
     //   showSnackbar();
     //   return;
     // }
-    const reviewdata ={
-      user_id:id,
-      project_type:selectedType,
-      reports_type: radiobutton ==="AnnotatationReports" ? "annotation" :"review",
-      start_date:format(selectRange[0].startDate, 'yyyy-MM-dd'),
-      end_date:format(selectRange[0].endDate, 'yyyy-MM-dd'),
+    const reviewdata = {
+      user_id: id,
+      project_type: selectedType,
+      reports_type: radiobutton === "AnnotatationReports" ? "annotation" : "review",
+      start_date: format(selectRange[0].startDate, 'yyyy-MM-dd'),
+      end_date: format(selectRange[0].endDate, 'yyyy-MM-dd'),
 
     }
     const progressObj = new GetUserAnalyticsAPI(reviewdata);
     dispatch(APITransport(progressObj));
     // setShowSpinner(true);
+    setTotalsummary(true)
+
   };
 
   const showSnackbar = () => {
@@ -192,7 +200,7 @@ const MyProgress = () => {
 
   const handleChangeReports = (e) => {
     setRadiobutton(e.target.value)
-}
+  }
 
 
   const renderToolBar = () => {
@@ -232,52 +240,45 @@ const MyProgress = () => {
   return (
     <ThemeProvider theme={themeDefault}>
       {/* <Header /> */}
+      {loading && <Spinner />}
       <Grid
         container
         direction="row"
-        // justifyContent="center"
-        // alignItems="center"
+        justifyContent="start"
+        alignItems="center"
+        sx={{ marginLeft: "50px" }}
       >
-      <Grid >
-             <Typography gutterBottom component="div" sx={{marginTop: "15px",fontSize:"16px"}}>
-             Select Report Type :
-             </Typography>
-           </Grid>
-                <FormControl>
+        <Grid >
+          <Typography gutterBottom component="div" sx={{ marginTop: "15px", fontSize: "16px" }}>
+            Select Report Type :
+          </Typography>
+        </Grid>
+        <FormControl>
 
-                    <RadioGroup
-                        row
-                        aria-labelledby="demo-row-radio-buttons-group-label"
-                        name="row-radio-buttons-group"
-                        sx={{ marginTop: "10px" }}
-                        value={radiobutton}
-                        onChange={handleChangeReports}
+          <RadioGroup
+            row
+            aria-labelledby="demo-row-radio-buttons-group-label"
+            name="row-radio-buttons-group"
+            sx={{ marginTop: "10px", marginLeft: "20px" }}
+            value={radiobutton}
+            onChange={handleChangeReports}
 
-                    >
-                        <FormControlLabel value="AnnotatationReports" control={<Radio />} label="Annotatation Reports"  />
-                        <FormControlLabel value="ReviewerReports" control={<Radio />} label="Reviewer Reports"  />
+          >
+            <FormControlLabel value="AnnotatationReports" control={<Radio />} label="Annotatation Reports" />
+            <FormControlLabel value="ReviewerReports" control={<Radio />} label="Reviewer Reports" />
 
-                    </RadioGroup>
-                </FormControl>
-                </Grid>
+          </RadioGroup>
+        </FormControl>
+      </Grid>
       <Grid
         container
         direction="row"
         justifyContent="center"
         alignItems="center"
       >
-        
-        <Grid container columnSpacing={4} rowSpacing={2} mt={1} mb={1}>
-          <Grid item xs={12} sm={12} md={3} lg={3} xl={3}>
-              <Button 
-                  endIcon={showPicker ? <ArrowRightIcon /> : <ArrowDropDownIcon />} 
-                  variant="contained" 
-                  color="primary" 
-                  onClick={() => setShowPicker(!showPicker)}
-              >
-               Pick Dates
-              </Button>
-          </Grid>
+
+        <Grid container columnSpacing={4} rowSpacing={2} mt={1} mb={1} justifyContent="space-evenly">
+
           <Grid item xs={12} sm={12} md={3} lg={3} xl={3}>
             <FormControl fullWidth size="small">
               <InputLabel id="project-type-label" sx={{ fontSize: "16px" }}>
@@ -297,6 +298,16 @@ const MyProgress = () => {
                 ))}
               </Select>
             </FormControl>
+          </Grid>
+          <Grid item xs={12} sm={12} md={3} lg={3} xl={3}>
+            <Button
+              endIcon={showPicker ? <ArrowRightIcon /> : <ArrowDropDownIcon />}
+              variant="contained"
+              color="primary"
+              onClick={() => setShowPicker(!showPicker)}
+            >
+              Pick Dates
+            </Button>
           </Grid>
           {/* <Grid item xs={12} sm={12} md={3} lg={3} xl={3}>
             <FormControl fullWidth size="small">
@@ -330,41 +341,136 @@ const MyProgress = () => {
             </Button>
           </Grid>
         </Grid>
-        {showPicker && <Box sx={{mt: 2, mb:2, display: "flex", justifyContent: "center", width: "100%"}}>
-            <Card sx={{overflowX: "scroll"}}>
-                <DateRangePicker
-                    onChange={handleRangeChange}
-                    staticRanges={[
-                        ...defaultStaticRanges,
-                        {
-                            label: "Till Date",
-                            range: () => ({
-                            startDate: new Date(Date.parse(UserDetails?.date_joined, 'yyyy-MM-ddTHH:mm:ss.SSSZ')),
-                            endDate: new Date(),
-                            }),
-                            isSelected(range) {
-                            const definedRange = this.range();
-                            return (
-                                isSameDay(range.startDate, definedRange.startDate) &&
-                                isSameDay(range.endDate, definedRange.endDate)
-                            );
-                            }
-                        },
-                    ]}
-                    showSelectionPreview={true}
-                    moveRangeOnFirstSelection={false}
-                    months={2}
-                    ranges={selectRange}
-                    minDate={new Date(Date.parse(UserDetails?.date_joined, 'yyyy-MM-ddTHH:mm:ss.SSSZ'))}
-                    maxDate={new Date()}
-                    direction="horizontal"
-                />
-        
-            </Card>
+        {showPicker && <Box sx={{ mt: 2, mb: 2, display: "flex", justifyContent: "center", width: "100%" }}>
+          <Card sx={{ overflowX: "scroll" }}>
+            <DateRangePicker
+              onChange={handleRangeChange}
+              staticRanges={[
+                ...defaultStaticRanges,
+                {
+                  label: "Till Date",
+                  range: () => ({
+                    startDate: new Date(Date.parse(UserDetails?.date_joined, 'yyyy-MM-ddTHH:mm:ss.SSSZ')),
+                    endDate: new Date(),
+                  }),
+                  isSelected(range) {
+                    const definedRange = this.range();
+                    return (
+                      isSameDay(range.startDate, definedRange.startDate) &&
+                      isSameDay(range.endDate, definedRange.endDate)
+                    );
+                  }
+                },
+              ]}
+              showSelectionPreview={true}
+              moveRangeOnFirstSelection={false}
+              months={2}
+              ranges={selectRange}
+              minDate={new Date(Date.parse(UserDetails?.date_joined, 'yyyy-MM-ddTHH:mm:ss.SSSZ'))}
+              maxDate={new Date()}
+              direction="horizontal"
+            />
 
-           
+          </Card>
+
+
         </Box>}
-        
+        {radiobutton === "AnnotatationReports" && totalsummary && <Grid
+          container
+          direction="row"
+          sx={{ mb: 3, mt: 2 }}
+        >
+          <Grid
+            container
+            alignItems="center"
+            direction="row"
+            justifyContent="flex-start"
+
+          >
+            <Typography variant="h6">Total Summary </Typography>
+
+          </Grid>
+
+          <Grid
+            container
+            alignItems="center"
+            direction="row"
+
+          >
+            <Typography variant="subtitle1">Annotated Tasks : </Typography>
+            <Typography variant="body2" className={classes.TotalSummarydata}>{UserAnalyticstotalsummary?.["Annotated Tasks"]}</Typography>
+          </Grid>
+          <Grid
+            container
+            alignItems="center"
+            direction="row"
+
+
+          >
+            <Typography variant="subtitle1">Average Annotation Time (In Seconds) : </Typography>
+            <Typography variant="body2" className={classes.TotalSummarydata}>{UserAnalyticstotalsummary?.["Average Annotation Time (In Seconds)"]}</Typography>
+          </Grid>
+          <Grid
+            container
+            alignItems="center"
+            direction="row"
+            justifyContent="flex-start"
+
+          >
+            <Typography variant="subtitle1">Word Count : </Typography>
+            <Typography variant="body2" className={classes.TotalSummarydata}>{UserAnalyticstotalsummary?.["Average Annotation Time (In Seconds)"]}</Typography>
+          </Grid>
+        </Grid>
+        }
+        {radiobutton === "ReviewerReports" && totalsummary && <Grid
+          container
+          alignItems="center"
+          direction="row"
+          sx={{ mb: 3, mt: 2 }}
+
+        >
+          <Grid
+            container
+            alignItems="center"
+            direction="row"
+            justifyContent="flex-start"
+
+          >
+            <Typography variant="h6">Total Summary </Typography>
+
+          </Grid>
+
+          <Grid
+            container
+            alignItems="center"
+            direction="row"
+            justifyContent="flex-start"
+
+          >
+            <Typography variant="subtitle1">Reviewed Tasks : </Typography>
+            <Typography variant="body2" className={classes.TotalSummarydata} >{UserAnalyticstotalsummary?.["Reviewed Tasks"]}</Typography>
+          </Grid>
+          <Grid
+            container
+            alignItems="center"
+            direction="row"
+            justifyContent="flex-start"
+
+          >
+            <Typography variant="subtitle1">Average Review Time (In Seconds) : </Typography>
+            <Typography variant="body2" className={classes.TotalSummarydata}>{UserAnalyticstotalsummary?.["Average Review Time (In Seconds)"]}</Typography>
+          </Grid>
+          <Grid
+            container
+            alignItems="center"
+            direction="row"
+            justifyContent="flex-start"
+
+          >
+            <Typography variant="subtitle1">Word Count : </Typography>
+            <Typography variant="body2" className={classes.TotalSummarydata}>{UserAnalyticstotalsummary?.["Word Count"]}</Typography>
+          </Grid>
+        </Grid>}
         {UserAnalytics?.length > 0 ? (
           <ThemeProvider theme={tableTheme}>
             <MUIDataTable
@@ -378,7 +484,7 @@ const MyProgress = () => {
           container
           justifyContent="center"
         >
-          <Grid item sx={{mt:"10%"}}>
+          <Grid item sx={{ mt: "10%" }}>
             {showSpinner ? <CircularProgress color="primary" size={50} /> : (
               !reportData?.length && submitted && <>No results</>
             )}
