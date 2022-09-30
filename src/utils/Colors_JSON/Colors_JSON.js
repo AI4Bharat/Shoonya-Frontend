3020cb40a2988220bda3fe64cdac698834cd5f8d
MrigankTarento <89840438+MrigankTarento@users.noreply.github.com>
2022-09-30 09:26:33 +0530

    changed colors according to shoonya logo

--- src/utils/Colors_JSON/Colors_JSON.js
+++ src/utils/Colors_JSON/Colors_JSON.js
@@ -1,79 +1,31 @@
 const colors = {
     orange: [
-        {
-            color: "rgb(204, 54, 0)"
-        },
-        {
-            color: "rgb(230, 115, 0)"
-        },
-        {
-            color: "rgb(204, 54, 0)"
-        },
-        {
-            color: "rgb(230, 126, 0)"
-        },
-        {
-            color: "rgb(255, 0, 0)"
-        },
-        {
-            color: "rgb(255, 105, 51)"
-        },
-        {
-            color: "rgb(255, 68, 0)"
-        },
-        {
-            color: "rgb(255, 140, 26)"
-        },
-        {
-            color: "rgb(255, 68, 0)"
-        },
-        {
-            color: "rgb(255, 152, 26)"
-        },
-        {
-            color: "rgb(255, 102, 102)"
-        },
-        {
-            color: "rgb(255, 175, 77)"
-        },
+        { color: "#e47e1b" },
+        { color: "#f7d8bb" },
+        { color: "#e47e1b" },
+        { color: "#e99849" },
+        { color: "#efb276" },
+        { color: "#e4af1b" },
+        { color: "#e4af1b" },
+        { color: "#ecc75f" },
+        { color: "#ea8c49" },
+        { color: "#f7d1b6" },
+        { color: "#ea9a76" },
+        { color: "#ded094" },
     ],
     green: [
-        {
-            color: "rgb(57, 106, 147)"
-        },
-        {
-            color: "rgb(7, 108, 171)"
-        },
-        {
-            color: "rgb(13, 49, 114)"
-        },
-        {
-            color: "rgb(0, 0, 204)"
-        },
-        {
-            color: "rgb(67, 109, 112)"
-        },
-        {
-            color: "rgb(0, 179, 179)"
-        },
-        {
-            color: "rgb(90, 145, 191)"
-        },
-        {
-            color: "rgb(35, 165, 246)"
-        },
-        {
-            color: "rgb(13, 49, 114)"
-        },
-        {
-            color: "rgb(128, 128, 255)"
-        },
-        {
-            color: "rgb(112, 165, 169)"
-        },
-        {
-            color: "rgb(0, 255, 255)"
-        },
+        { color: "#4c943a" },
+        { color: "#356829" },
+        { color: "#a6ca9d" },
+        { color: "#80e466" },
+        { color: "#b3efa3" },
+        { color: "#4fec26" },
+        { color: "#3e7a2d" },
+        { color: "#629a6a" },
+        { color: "#456c4a" },
+        { color: "#a1c2a6" },
+        { color: "#508658" },
+        { color: "#499136" },
     ]
 };
 
