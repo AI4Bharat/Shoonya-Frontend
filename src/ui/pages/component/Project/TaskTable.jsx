97acf69d0df1ffab4b1c645960dce2ec062b2b96
ganavikumarswamy <ganavi.kumarswamy@tarento.com>
2022-12-13 15:35:01 +0530

    .

--- src/ui/pages/component/Project/TaskTable.jsx
+++ src/ui/pages/component/Project/TaskTable.jsx
@@ -267,7 +267,7 @@ const TaskTable = (props) => {
                 </Link>);
                 return row;
             })
-            const email =  props.type === "review"&& selectedFilters.task_status=== "accepted_with_changes" || selectedFilters.task_status=== "to_be_revised" ? "email" : ""
+            const email =  props.type === "review"&& selectedFilters.task_status=== "accepted_with_changes" || selectedFilters.task_status=== "to_be_revised" ? "Annotator Email" : ""
             let colList = ["id", ... !!email ? [email] : []];
             colList.push(...Object.keys(taskList[0].data).filter(el => !excludeCols.includes(el)));
             taskList[0].task_status && colList.push("status");
