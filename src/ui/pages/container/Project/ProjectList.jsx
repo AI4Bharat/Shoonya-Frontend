4359706d3f62e78c8b92c81890b24de6744075e0
ganavikumarswamy <ganavi.kumarswamy@tarento.com>
2023-02-08 16:38:12 +0530

    added filter to project list

--- src/ui/pages/container/Project/ProjectList.jsx
+++ src/ui/pages/container/Project/ProjectList.jsx
@@ -1,81 +1,89 @@
 import React, { useEffect, useState } from "react";
-import { Radio, Box, Grid, Typography, ThemeProvider } from '@mui/material';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
+import { Radio, Box, Grid, Typography, ThemeProvider } from "@mui/material";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
 import ProjectCardList from "./ProjectCardList";
 import ProjectCard from "./ProjectCard";
-import Spinner from "../../component/common/Spinner"
+import Spinner from "../../component/common/Spinner";
 import GetProjectsAPI from "../../../../redux/actions/api/Dashboard/GetProjects";
-import APITransport from '../../../../redux/actions/apitransport/apitransport';
-import { useDispatch, useSelector } from 'react-redux';
+import APITransport from "../../../../redux/actions/apitransport/apitransport";
+import { useDispatch, useSelector } from "react-redux";
 import Search from "../../component/common/Search";
 import DatasetStyle from "../../../styles/Dataset";
 import themeDefault from "../../../theme/theme";
 import tableTheme from "../../../theme/tableTheme";
 
-
 export default function ProjectList() {
-    const [radiobutton, setRadiobutton] = useState(true)
-    const classes = DatasetStyle();
-    const dispatch = useDispatch();
-    const [loading, setLoading] = useState(false);
-    const apiLoading = useSelector(state => state.apiStatus.loading);
-    const projectData = useSelector(state => state.getProjects.data);
+  const [radiobutton, setRadiobutton] = useState(true);
+  const classes = DatasetStyle();
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
+  const [selectedFilters, setsSelectedFilters] = useState({
+    project_type: "",
+    project_user_type: "",
+    archived_projects: "",
+  });
+  const apiLoading = useSelector((state) => state.apiStatus.loading);
+  const projectData = useSelector((state) => state.getProjects.data);
+console.log(selectedFilters.project_type,"selectedTypeselectedType")
+  const getDashboardprojectData = () => {
+    setLoading(true);
+    const projectObj = new GetProjectsAPI(selectedFilters);
+    dispatch(APITransport(projectObj));
+  };
 
-    const getDashboardprojectData = () => {
-        setLoading(true);
-        const projectObj = new GetProjectsAPI();
-        dispatch(APITransport(projectObj));
-    }
+  useEffect(() => {
+    setLoading(false);
+  }, [projectData]);
 
-    useEffect(() => {
-        setLoading(false);
-    }, [projectData])
+  useEffect(() => {
+    getDashboardprojectData();
+  }, [selectedFilters]);
 
-    useEffect(() => {
-        getDashboardprojectData();
+  const handleProjectlist = () => {
+    setRadiobutton(true);
+  };
+  const handleProjectcard = () => {
+    setRadiobutton(false);
+  };
 
-    }, []);
+  return (
+    <ThemeProvider theme={themeDefault}>
+      {loading && <Spinner />}
 
-    const handleProjectlist = () => {
-        setRadiobutton(true)
+      {/* <Grid container direction="row" columnSpacing={3} rowSpacing={2} sx={{ position: "static", bottom: "-51px", left: "20px" }} > */}
+      <Grid container className={classes.root}>
+        <Grid item style={{ flexGrow: "0" }}>
+          <Typography variant="h6" sx={{ paddingBottom: "7px" }}>
+            View :{" "}
+          </Typography>
+        </Grid>
+        <Grid item style={{ flexGrow: "1", paddingLeft: "5px" }}>
+          <FormControl>
+            <RadioGroup
+              row
+              aria-labelledby="demo-row-radio-buttons-group-label"
+              name="row-radio-buttons-group"
+              defaultValue="ProjectList"
+            >
+              <FormControlLabel
+                value="ProjectList"
+                control={<Radio />}
+                label="List"
+                onClick={handleProjectlist}
+              />
+              <FormControlLabel
+                value="ProjectCard"
+                control={<Radio />}
+                label="Card"
+                onClick={handleProjectcard}
+              />
+            </RadioGroup>
+          </FormControl>
+        </Grid>
 
-    }
-    const handleProjectcard = () => {
-        setRadiobutton(false)
-    }
-
-
-    return (
-        <ThemeProvider theme={themeDefault}>
-            {loading && <Spinner />}
-
-           
-            {/* <Grid container direction="row" columnSpacing={3} rowSpacing={2} sx={{ position: "static", bottom: "-51px", left: "20px" }} > */}
-            <Grid container className={classes.root} >
-            <Grid item style={{ flexGrow: "0"}}>
-					<Typography variant="h6" sx={{paddingBottom:"7px"}}>View :  </Typography>
-                    
-				</Grid>
-                <Grid item style={{ flexGrow: "1",paddingLeft: "5px"}}>
-                <FormControl >
-                        <RadioGroup
-                            row
-                            aria-labelledby="demo-row-radio-buttons-group-label"
-                            name="row-radio-buttons-group"
-                            defaultValue="ProjectList"
-                            
-                        >
-
-                            <FormControlLabel value="ProjectList" control={<Radio />} label="List" onClick={handleProjectlist} />
-                            <FormControlLabel value="ProjectCard" control={<Radio />} label="Card" onClick={handleProjectcard} />
-
-                        </RadioGroup>
-                    </FormControl>
-				</Grid>
-                
-                {/* <Grid  >
+        {/* <Grid  >
                     <FormControl >
                         <RadioGroup
                             row
@@ -92,18 +100,26 @@ export default function ProjectList() {
                     </FormControl>
                 </Grid>
             </Grid> */}
-            <Grid xs={3} item className={classes.fixedWidthContainer}>
-            <Search />
-				</Grid>
-                </Grid>
-            {/* <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+        <Grid xs={3} item className={classes.fixedWidthContainer}>
+          <Search />
+        </Grid>
+      </Grid>
+      {/* <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
                 <Search />
             </Grid> */}
-            <Box >
-                <Box sx={{ marginTop: "20px" }}>
-                    {radiobutton ? <ProjectCardList projectData={projectData} /> : <ProjectCard projectData={projectData} />}
-                </Box>
-            </Box>
-        </ThemeProvider>
-    );
+      <Box>
+        <Box sx={{ marginTop: "20px" }}>
+          {radiobutton ? (
+            <ProjectCardList
+              projectData={projectData}
+              selectedFilters={selectedFilters} 
+              setsSelectedFilters={setsSelectedFilters} 
+            />
+          ) : (
+            <ProjectCard projectData={projectData} />
+          )}
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
 }
