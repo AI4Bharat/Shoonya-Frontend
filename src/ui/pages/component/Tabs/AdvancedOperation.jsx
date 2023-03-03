16bbaf574d5e1f45cf70e0f83257cd39e7aa4a21
ganavikumarswamy <ganavi.kumarswamy@tarento.com>
2023-03-03 14:45:12 +0530

    Deallocation Annotators And Reviewers  visable to manager and owner

--- src/ui/pages/component/Tabs/AdvancedOperation.jsx
+++ src/ui/pages/component/Tabs/AdvancedOperation.jsx
@@ -548,7 +548,7 @@ const AdvancedOperation = (props) => {
           <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
             <DeleteProjectTasks />
           </Grid>
-          {loggedInUserData?.role === 2 && (<Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+          {loggedInUserData?.role !== 1 && (<Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
             <DeallocationAnnotatorsAndReviewers />
           </Grid>)}
           
