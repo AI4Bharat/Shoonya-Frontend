055afe46ee9702d39a14749d0f0592b3b35abaa4
ganavikumarswamy <ganavi.kumarswamy@tarento.com>
2023-03-03 14:39:35 +0530

    .

--- src/ui/pages/component/Project/MembersTable.jsx
+++ src/ui/pages/component/Project/MembersTable.jsx
@@ -16,319 +16,355 @@ import addUserTypes from "../../../../constants/addUserTypes";
 import { useNavigate, useParams } from "react-router-dom";
 import { ThemeProvider, Grid } from "@mui/material";
 import tableTheme from "../../../theme/tableTheme";
-import RemoveProjectMemberAPI from '../../../../redux/actions/api/ProjectDetails/RemoveProjectMember';
-import RemoveProjectReviewerAPI from '../../../../redux/actions/api/ProjectDetails/RemoveProjectReviewer';
+import RemoveProjectMemberAPI from "../../../../redux/actions/api/ProjectDetails/RemoveProjectMember";
+import RemoveProjectReviewerAPI from "../../../../redux/actions/api/ProjectDetails/RemoveProjectReviewer";
 import CustomizedSnackbars from "../../component/common/Snackbar";
 import Search from "../../component/common/Search";
+import RemoveFrozenUserAPI from "../../../../redux/actions/api/ProjectDetails/RemoveFrozenUser";
 
 
 const columns = [
-    {
-        name: "Name",
-        label: "Name",
-        options: {
-            filter: false,
-            sort: false,
-            align: "center",
-            setCellHeaderProps: sort => ({ style: { height: "70px", padding: "16px" } }),
-        },
+  {
+    name: "Name",
+    label: "Name",
+    options: {
+      filter: false,
+      sort: false,
+      align: "center",
+      setCellHeaderProps: (sort) => ({
+        style: { height: "70px", padding: "16px" },
+      }),
     },
-    {
-        name: "Email",
-        label: "Email",
-        options: {
-            filter: false,
-            sort: false,
-        },
+  },
+  {
+    name: "Email",
+    label: "Email",
+    options: {
+      filter: false,
+      sort: false,
     },
-    {
-        name: "Role",
-        label: "Role",
-        options: {
-            filter: false,
-            sort: false,
-        },
+  },
+  {
+    name: "Role",
+    label: "Role",
+    options: {
+      filter: false,
+      sort: false,
     },
-    {
-        name: "Actions",
-        label: "Actions",
-        options: {
-            filter: false,
-            sort: false,
-        },
+  },
+  {
+    name: "Actions",
+    label: "Actions",
+    options: {
+      filter: false,
+      sort: false,
     },
+  },
 ];
 
 const options = {
-    filterType: "checkbox",
-    selectableRows: "none",
-    download: false,
-    filter: false,
-    print: false,
-    search: false,
-    viewColumns: false,
-    jumpToPage: true,
+  filterType: "checkbox",
+  selectableRows: "none",
+  download: false,
+  filter: false,
+  print: false,
+  search: false,
+  viewColumns: false,
+  jumpToPage: true,
 };
 
 const addLabel = {
-    organization: "Invite Users to Organization",
-    [addUserTypes.PROJECT_ANNOTATORS]: "Add Annotators to Project",
-    [addUserTypes.PROJECT_REVIEWER]: "Add Reviewers to Project",
-}
+  organization: "Invite Users to Organization",
+  [addUserTypes.PROJECT_ANNOTATORS]: "Add Annotators to Project",
+  [addUserTypes.PROJECT_REVIEWER]: "Add Reviewers to Project",
+};
 
 const MembersTable = (props) => {
-    const [addUserDialogOpen, setAddUserDialogOpen] = useState(false);
-    const { orgId, id } = useParams();
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const [userRole, setUserRole] = useState();
-    const [loading, setLoading] = useState(false);
-    const { dataSource, hideButton, onRemoveSuccessGetUpdatedMembers } = props;
-    const [snackbar, setSnackbarInfo] = useState({
-        open: false,
-        message: "",
-        variant: "success",
+  const [addUserDialogOpen, setAddUserDialogOpen] = useState(false);
+  const { orgId, id } = useParams();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [userRole, setUserRole] = useState();
+  const [loading, setLoading] = useState(false);
+  const { dataSource, hideButton, onRemoveSuccessGetUpdatedMembers } = props;
+  const [snackbar, setSnackbarInfo] = useState({
+    open: false,
+    message: "",
+    variant: "success",
+  });
+  const userDetails = useSelector((state) => state.fetchLoggedInUserData.data);
+  const ProjectDetails = useSelector((state) => state.getProjectDetails.data);
+  const apiLoading = useSelector((state) => state.apiStatus.loading);
+  const SearchWorkspaceMembers = useSelector(
+    (state) => state.SearchProjectCards.data
+  );
+  console.log(ProjectDetails.frozen_users, "ProjectDetailsProjectDetails");
+  const pageSearch = () => {
+    return dataSource.filter((el) => {
+      if (SearchWorkspaceMembers == "") {
+        return el;
+      } else if (
+        el.username
+          ?.toLowerCase()
+          .includes(SearchWorkspaceMembers?.toLowerCase())
+      ) {
+        return el;
+      } else if (
+        el.email?.toLowerCase().includes(SearchWorkspaceMembers?.toLowerCase())
+      ) {
+        return el;
+      }
     });
-    const userDetails = useSelector(state => state.fetchLoggedInUserData.data);
-    const ProjectDetails = useSelector(state => state.getProjectDetails.data);
-    const apiLoading = useSelector(state => state.apiStatus.loading);
-    const SearchWorkspaceMembers = useSelector((state) => state.SearchProjectCards.data);
-    console.log(dataSource,"dataSource")
-    const pageSearch = () => {
-       
-        return dataSource.filter((el) => {
+  };
 
-            if (SearchWorkspaceMembers == "") {
+  useEffect(() => {
+    userDetails && setUserRole(userDetails.role);
+  }, []);
 
-                return el;
-            } else if (
-                el.username
-                    ?.toLowerCase()
-                    .includes(SearchWorkspaceMembers?.toLowerCase())
-            ) {
+  const handleUserDialogClose = () => {
+    setAddUserDialogOpen(false);
+  };
 
-                return el;
-            }
-            else if (
-                el.email
-                    ?.toLowerCase()
-                    .includes(SearchWorkspaceMembers?.toLowerCase())
-            ) {
+  const handleUserDialogOpen = () => {
+    setAddUserDialogOpen(true);
+  };
+  // const Projectdata = ProjectDetails && ProjectDetails.filter((el,i)=>{
+  //     return [
+  //                 el.email,
 
-                return el;
-            }
-
-        }
-        )
-
+  //             ]
+  // });
+  const handleProjectMember = async (userid) => {
+    const projectObj = new RemoveProjectMemberAPI(id, { ids: [userid] });
+    //dispatch(APITransport(projectObj));
+    const res = await fetch(projectObj.apiEndPoint(), {
+      method: "POST",
+      body: JSON.stringify(projectObj.getBody()),
+      headers: projectObj.getHeaders().headers,
+    });
+    const resp = await res.json();
+    setLoading(false);
+    if (res.ok) {
+      setSnackbarInfo({
+        open: true,
+        message: resp?.message,
+        variant: "success",
+      });
+      onRemoveSuccessGetUpdatedMembers();
+    } else {
+      setSnackbarInfo({
+        open: true,
+        message: resp?.message,
+        variant: "error",
+      });
     }
-
-
-    useEffect(() => {
-        userDetails && setUserRole(userDetails.role);
-    }, [])
-
-    const handleUserDialogClose = () => {
-        setAddUserDialogOpen(false);
-    };
-
-    const handleUserDialogOpen = () => {
-        setAddUserDialogOpen(true);
-    };
-    // const Projectdata = ProjectDetails && ProjectDetails.filter((el,i)=>{
-    //     return [
-    //                 el.email, 
-
-    //             ]
-    // });
-    const handleProjectMember = async (userid) => {
-        const projectObj = new RemoveProjectMemberAPI(id, { ids: [userid] });
-        //dispatch(APITransport(projectObj));
-        const res = await fetch(projectObj.apiEndPoint(), {
-            method: "POST",
-            body: JSON.stringify(projectObj.getBody()),
-            headers: projectObj.getHeaders().headers,
-        });
-        const resp = await res.json();
-        setLoading(false);
-        if (res.ok) {
-            setSnackbarInfo({
-                open: true,
-                message: resp?.message,
-                variant: "success",
-            })
-            onRemoveSuccessGetUpdatedMembers()
-        } else {
-            setSnackbarInfo({
-                open: true,
-                message: resp?.message,
-                variant: "error",
-            })
-        }
-
+  };
+  const handleProjectReviewer = async (Projectid) => {
+    const projectObj = new RemoveProjectReviewerAPI(id, { ids: [Projectid] });
+    // dispatch(APITransport(projectObj));
+    const res = await fetch(projectObj.apiEndPoint(), {
+      method: "POST",
+      body: JSON.stringify(projectObj.getBody()),
+      headers: projectObj.getHeaders().headers,
+    });
+    const resp = await res.json();
+    setLoading(false);
+    if (res.ok) {
+      setSnackbarInfo({
+        open: true,
+        message: resp?.message,
+        variant: "success",
+      });
+      onRemoveSuccessGetUpdatedMembers();
+    } else {
+      setSnackbarInfo({
+        open: true,
+        message: resp?.message,
+        variant: "error",
+      });
     }
-    const handleProjectReviewer = async (Projectid) => {
-        const projectObj = new RemoveProjectReviewerAPI(id, { ids: [Projectid] });
-       // dispatch(APITransport(projectObj));
-        const res = await fetch(projectObj.apiEndPoint(), {
-            method: "POST",
-            body: JSON.stringify(projectObj.getBody()),
-            headers: projectObj.getHeaders().headers,
-        });
-        const resp = await res.json();
-        setLoading(false);
-        if (res.ok) {
-            setSnackbarInfo({
-                open: true,
-                message: resp?.message,
-                variant: "success",
-            })
-            onRemoveSuccessGetUpdatedMembers()
-        } else {
-            setSnackbarInfo({
-                open: true,
-                message: resp?.message,
-                variant: "error",
-            })
-        }
-    }
-    useEffect(() => {
-        setLoading(apiLoading);
-    }, [apiLoading])
+  };
 
-    const projectlist = (el) => {
-        let temp = false;
-        ProjectDetails?.frozen_users.forEach((em) => {
-            if (el == em.id) {
-                temp = true
-            }
-        })
-        return temp;
-    }
-    const data =
-        dataSource && dataSource.length > 0
-            ? pageSearch().map((el, i) => {
-                const userRole = el.role && UserMappedByRole(el.role).element;
+  const handleRemoveFrozenUsers = async(FrozenUserId) => {
+    const projectObj = new RemoveFrozenUserAPI(id, { ids: [FrozenUserId] });
+    //dispatch(APITransport(projectObj));
+    const res = await fetch(projectObj.apiEndPoint(), {
+      method: "POST",
+      body: JSON.stringify(projectObj.getBody()),
+      headers: projectObj.getHeaders().headers,
+  });
+  const resp = await res.json();
+  // setLoading(false);
+  if (res.ok) {
+      setSnackbarInfo({
+          open: true,
+          message: resp?.message,
+          variant: "success",
+      })
+      onRemoveSuccessGetUpdatedMembers();
+  } else {
+      setSnackbarInfo({
+          open: true,
+          message: resp?.message,
+          variant: "error",
+      })
+  }
+  };
 
 
+  useEffect(() => {
+    setLoading(apiLoading);
+  }, [apiLoading]);
 
-                return [
-                    el.username,
-                    el.email,
-                    userRole ? userRole : el.role,
-                    <>
-                        <CustomButton
-                            sx={{ p: 1, borderRadius: 2 }}
-                            onClick={() => {
-                                navigate(`/profile/${el.id}`);
-                            }}
-                            label={"View"}
-                        />
+  const projectlist = (el) => {
+    let temp = false;
+    ProjectDetails?.frozen_users.forEach((em) => {
+      if (el == em.id) {
+        temp = true;
+      }
+    });
+    return temp;
+  };
+  const data =
+    dataSource && dataSource.length > 0
+      ? pageSearch().map((el, i) => {
+          const userRole = el.role && UserMappedByRole(el.role).element;
 
-                        {props.type === addUserTypes.PROJECT_ANNOTATORS &&
-                            <CustomButton
-                                sx={{ borderRadius: 2, backgroundColor: "#cf5959", m: 1, height: "40px" }}
-                                label="Remove"
-                                onClick={() => handleProjectMember(el.id)}
-                                disabled={projectlist(el.id)}
+          return [
+            el.username,
+            el.email,
+            userRole ? userRole : el.role,
+            <>
+              <CustomButton
+                sx={{ p: 1, borderRadius: 2 }}
+                onClick={() => {
+                  navigate(`/profile/${el.id}`);
+                }}
+                label={"View"}
+              />
 
-                            />}
-                        {props.type === addUserTypes.PROJECT_REVIEWER &&
-                            <CustomButton
-                                sx={{ borderRadius: 2, backgroundColor: "#cf5959", m: 1, height: "40px" }}
-                                label="Remove"
-                                onClick={() => handleProjectReviewer(el.id)}
-                                disabled={projectlist(el.id)}
-                            />}
-                    </>
-
-                ];
-            })
-            : [];
-    const options = {
-        textLabels: {
-            body: {
-                noMatch: "No records",
-            },
-            toolbar: {
-                search: "Search",
-                viewColumns: "View Column",
-            },
-            pagination: { rowsPerPage: "Rows per page" },
-            options: { sortDirection: "desc" },
-        },
-        // customToolbar: fetchHeaderButton,
-        displaySelectToolbar: false,
-        fixedHeader: false,
-        filterType: "checkbox",
-        download: false,
-        print: false,
-        rowsPerPageOptions: [10, 25, 50, 100],
-        // rowsPerPage: PageInfo.count,
-        filter: false,
-        // page: PageInfo.page,
-        viewColumns: false,
-        selectableRows: "none",
-        search: false,
-        jumpToPage: true,
-    };
-    const renderSnackBar = () => {
-        return (
-            <CustomizedSnackbars
-                open={snackbar.open}
-                handleClose={() =>
-                    setSnackbarInfo({ open: false, message: "", variant: "" })
-                }
-                anchorOrigin={{ vertical: "top", horizontal: "right" }}
-                variant={snackbar.variant}
-                message={snackbar.message}
-            />
-        );
-    };
-
-    return (
-        <React.Fragment  >
-            {userRole !== 1 && !hideButton ?
+              {props.type === addUserTypes.PROJECT_ANNOTATORS && (
+                <>
+                  <CustomButton
+                    sx={{
+                      borderRadius: 2,
+                      backgroundColor: "#cf5959",
+                      m: 1,
+                      height: "40px",
+                    }}
+                    label="Remove"
+                    onClick={() => handleProjectMember(el.id)}
+                    disabled={projectlist(el.id)}
+                  />
+                  {ProjectDetails.frozen_users && ProjectDetails.frozen_users.length > 0 &&
+                  <CustomButton
+                    sx={{ borderRadius: 2, backgroundColor: "#cf5959" }}
+                    label="Remove"
+                    onClick={() => handleRemoveFrozenUsers(el.id)}
+                  />}
+                </>
+              )}
+              {props.type === addUserTypes.PROJECT_REVIEWER && (
                 <CustomButton
-                    sx={{ borderRadius: 2,  whiteSpace: "nowrap" }}
-                    startIcon={<PersonAddAlt />}
-                    label={props.type ? addLabel[props.type] : "Add Users"}
-                    fullWidth
-                    onClick={handleUserDialogOpen}
-                /> : null
-            }
-            {props.type === "organization"  ?
-                <InviteUsersDialog
-                    handleDialogClose={handleUserDialogClose}
-                    isOpen={addUserDialogOpen}
-                    id={orgId}
+                  sx={{
+                    borderRadius: 2,
+                    backgroundColor: "#cf5959",
+                    m: 1,
+                    height: "40px",
+                  }}
+                  label="Remove"
+                  onClick={() => handleProjectReviewer(el.id)}
+                  disabled={projectlist(el.id)}
                 />
-                :
-                <AddUsersDialog
-                    handleDialogClose={handleUserDialogClose}
-                    isOpen={addUserDialogOpen}
-                    userType={props.type}
-                    id={id}
-                />
-            }
-            {renderSnackBar()}
-            {(props.type === "organization" || hideButton)  &&
-                <Grid sx={{ mb: 1 }}>
-                    <Search />
-                </Grid>
-            }
-
-            <ThemeProvider theme={tableTheme} sx={{ marginTop: "20px" }} >
-                <MUIDataTable
-                    title={""}
-                    data={data}
-                    columns={columns}
-                    options={options}
-                // filter={false}
-                />
-            </ThemeProvider>
-        </React.Fragment>
+              )}
+            </>,
+          ];
+        })
+      : [];
+  const options = {
+    textLabels: {
+      body: {
+        noMatch: "No records",
+      },
+      toolbar: {
+        search: "Search",
+        viewColumns: "View Column",
+      },
+      pagination: { rowsPerPage: "Rows per page" },
+      options: { sortDirection: "desc" },
+    },
+    // customToolbar: fetchHeaderButton,
+    displaySelectToolbar: false,
+    fixedHeader: false,
+    filterType: "checkbox",
+    download: false,
+    print: false,
+    rowsPerPageOptions: [10, 25, 50, 100],
+    // rowsPerPage: PageInfo.count,
+    filter: false,
+    // page: PageInfo.page,
+    viewColumns: false,
+    selectableRows: "none",
+    search: false,
+    jumpToPage: true,
+  };
+  const renderSnackBar = () => {
+    return (
+      <CustomizedSnackbars
+        open={snackbar.open}
+        handleClose={() =>
+          setSnackbarInfo({ open: false, message: "", variant: "" })
+        }
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        variant={snackbar.variant}
+        message={snackbar.message}
+      />
     );
+  };
+
+  return (
+    <React.Fragment>
+      {userRole !== 1 && !hideButton ? (
+        <CustomButton
+          sx={{ borderRadius: 2, whiteSpace: "nowrap" }}
+          startIcon={<PersonAddAlt />}
+          label={props.type ? addLabel[props.type] : "Add Users"}
+          fullWidth
+          onClick={handleUserDialogOpen}
+        />
+      ) : null}
+      {props.type === "organization" ? (
+        <InviteUsersDialog
+          handleDialogClose={handleUserDialogClose}
+          isOpen={addUserDialogOpen}
+          id={orgId}
+        />
+      ) : (
+        <AddUsersDialog
+          handleDialogClose={handleUserDialogClose}
+          isOpen={addUserDialogOpen}
+          userType={props.type}
+          id={id}
+        />
+      )}
+      {renderSnackBar()}
+      {(props.type === "organization" || hideButton) && (
+        <Grid sx={{ mb: 1 }}>
+          <Search />
+        </Grid>
+      )}
+
+      <ThemeProvider theme={tableTheme} sx={{ marginTop: "20px" }}>
+        <MUIDataTable
+          title={""}
+          data={data}
+          columns={columns}
+          options={options}
+          // filter={false}
+        />
+      </ThemeProvider>
+    </React.Fragment>
+  );
 };
 
 export default MembersTable;
