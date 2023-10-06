c76f06cd27f798786fec4609fc8ee39f6bb29818
kartikvirendrar <kartikrajput357@gmail.com>
2023-10-06 17:57:25 +0530

    send email for org project reports

--- src/redux/actions/api/Organization/GetOrganizationProjectReports.js
+++ src/redux/actions/api/Organization/GetOrganizationProjectReports.js
@@ -6,12 +6,13 @@ import ENDPOINTS from "../../../../config/apiendpoint";
 import constants from "../../../constants";
 
 export default class GetOrganizationProjectReportsAPI extends API {
-  constructor(orgId, projectType, targetLanguage , sortByColumn, descOrder, timeout = 2000) {
+  constructor(orgId, projectType, targetLanguage , sortByColumn, descOrder, timeout = 2000, emailId) {
     super("POST", timeout, false);
     this.projectType = projectType;
     this.targetLanguage = targetLanguage === "all" ? undefined : targetLanguage;
     this.sortByColumn = sortByColumn ?? undefined;
     this.descOrder = descOrder ?? undefined;
+    this.emailId = emailId;
     this.type = constants.GET_ORGANIZATION_PROJECT_REPORTS;
     this.endpoint = `${super.apiEndPointAuto()}${ENDPOINTS.getOrganizations}${orgId}/project_analytics/`;
   }
@@ -33,7 +34,7 @@ export default class GetOrganizationProjectReportsAPI extends API {
       tgt_language: this.targetLanguage,
       sort_by_column_name: this.sortByColumn,
       descending_order: this.descOrder,
-     
+      email_id: this.emailId,
     };
   }
 
