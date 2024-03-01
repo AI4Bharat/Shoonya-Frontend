50a640a4177038221a501c528a0aa0330594704e
Pursottam6003 <rahulsah6003@gmail.com>
2024-03-01 13:33:22 +0530

    updated the deallocation by task id for annotators

--- src/ui/pages/component/Tabs/AdvancedOperation.jsx
+++ src/ui/pages/component/Tabs/AdvancedOperation.jsx
@@ -46,9 +46,8 @@ import getDownloadProjectAnnotationsAPI from "../../../../redux/actions/api/Proj
 import DeallocationAnnotatorsAndReviewers from "../../container/Project/DeallocationAnnotatorsAndReviewers";
 import SuperCheckSettings from "../../container/Project/SuperCheckSettings";
 import userRole from "../../../../utils/UserMappedByRole/Roles";
-import TextField from '@mui/material/TextField';
+import TextField from "@mui/material/TextField";
 import LoginAPI from "../../../../redux/actions/api/UserManagement/Login";
-
 
 const ProgressType = [
   "incomplete",
@@ -57,7 +56,11 @@ const ProgressType = [
   "super_checked",
   "exported",
 ];
-const projectStage = [{ name: "Annotation Stage", value: 1, disabled: false }, { name: "Review Stage", value: 2, disabled: false }, { name: "Super Check Stage", value: 3, disabled: false }]
+const projectStage = [
+  { name: "Annotation Stage", value: 1, disabled: false },
+  { name: "Review Stage", value: 2, disabled: false },
+  { name: "Super Check Stage", value: 3, disabled: false },
+];
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
 const MenuProps = {
@@ -97,7 +100,7 @@ const AdvancedOperation = (props) => {
   const [OpenExportProjectDialog, setOpenExportProjectDialog] = useState(false);
   const [datasetId, setDatasetId] = useState("");
   const [projectType, setProjectType] = useState("");
-  const [taskReviews, setTaskReviews] = useState("")
+  const [taskReviews, setTaskReviews] = useState("");
   const { id } = useParams();
   const classes = DatasetStyle();
   const dispatch = useDispatch();
@@ -113,26 +116,32 @@ const AdvancedOperation = (props) => {
   );
 
   const isSuperChecker =
-    ((userRole.WorkspaceManager === loggedInUserData?.role ||
-      userRole.OrganizationOwner === loggedInUserData?.role ||
-      userRole.Admin === loggedInUserData?.role) ? ProjectDetails?.project_stage == 3 : false ||
-    ProjectDetails?.review_supercheckers?.some(
-      (superchecker) => superchecker.id === loggedInUserData?.id
-    ));
+    userRole.WorkspaceManager === loggedInUserData?.role ||
+    userRole.OrganizationOwner === loggedInUserData?.role ||
+    userRole.Admin === loggedInUserData?.role
+      ? ProjectDetails?.project_stage == 3
+      : false ||
+        ProjectDetails?.review_supercheckers?.some(
+          (superchecker) => superchecker.id === loggedInUserData?.id
+        );
 
-  const [taskStatus, setTaskStatus] = useState(isSuperChecker ? ["incomplete", "annotated", "reviewed", "super_checked", "exported",] : ["incomplete", "annotated", "reviewed", "super_checked", "exported",]);
+  const [taskStatus, setTaskStatus] = useState(
+    isSuperChecker
+      ? ["incomplete", "annotated", "reviewed", "super_checked", "exported"]
+      : ["incomplete", "annotated", "reviewed", "super_checked", "exported"]
+  );
 
-  let ProgressTypeValue = "super_checked"
-  const filterdata = ProgressType.filter(item => item !== ProgressTypeValue)
-  const FilterProgressType = isSuperChecker ? ProgressType : filterdata
+  let ProgressTypeValue = "super_checked";
+  const filterdata = ProgressType.filter((item) => item !== ProgressTypeValue);
+  const FilterProgressType = isSuperChecker ? ProgressType : filterdata;
 
   const getProjectDetails = () => {
     const projectObj = new GetProjectDetailsAPI(id);
     dispatch(APITransport(projectObj));
   };
   useEffect(() => {
-    setProjectType(ProjectTypes.input_dataset?.class)
-  }, [ProjectTypes])
+    setProjectType(ProjectTypes.input_dataset?.class);
+  }, [ProjectTypes]);
 
   useEffect(() => {
     getProjectDetails();
@@ -143,7 +152,7 @@ const AdvancedOperation = (props) => {
       title: ProjectDetails.title,
       description: ProjectDetails.description,
     });
-    setTaskReviews(ProjectDetails.project_stage)
+    setTaskReviews(ProjectDetails.project_stage);
   }, [ProjectDetails]);
 
   useEffect(() => {
@@ -151,17 +160,16 @@ const AdvancedOperation = (props) => {
     dispatch(APITransport(typesObj));
   }, []);
 
-
   const getExportProjectButton = async () => {
     setOpenExportProjectDialog(false);
     const projectObj =
       ProjectTypes?.output_dataset?.save_type === "new_record"
         ? new GetExportProjectButtonAPI(
-          id,
-          ProjectDetails?.datasets[0].instance_id,
-          datasetId,
-          ProjectTypes?.output_dataset?.save_type
-        )
+            id,
+            ProjectDetails?.datasets[0].instance_id,
+            datasetId,
+            ProjectTypes?.output_dataset?.save_type
+          )
         : new GetExportProjectButtonAPI(id);
     //dispatch(APITransport(projectObj));
     const res = await fetch(projectObj.apiEndPoint(), {
@@ -213,39 +221,33 @@ const AdvancedOperation = (props) => {
     //     variant: "error",
     //   });
     // }
-
-
-  }
+  };
 
   const handleReviewToggle = async (e) => {
-    let ProjectStageValue = e.target.value
-    setTaskReviews(e.target.value)
+    let ProjectStageValue = e.target.value;
+    setTaskReviews(e.target.value);
 
     if (ProjectStageValue === 1) {
       const disableSuperchecker = [...projectStage].map((opt) => {
         if (opt.value === 3) opt.disabled = true;
         else opt.disabled = false;
         return opt;
-      })
+      });
 
       setTaskReviews(disableSuperchecker);
-    }
-    else if (ProjectStageValue === 2) {
+    } else if (ProjectStageValue === 2) {
       const disableSuperchecker = [...projectStage].map((opt) => {
         if (opt.value === 3) opt.disabled = false;
         else opt.disabled = false;
         return opt;
-
       });
 
       setTaskReviews(disableSuperchecker);
-    }
-    else if (ProjectStageValue === 3) {
+    } else if (ProjectStageValue === 3) {
       const disableSuperchecker = [...projectStage].map((opt) => {
         if (opt.value === 1) opt.disabled = true;
         else opt.disabled = false;
         return opt;
-
       });
 
       setTaskReviews(disableSuperchecker);
@@ -279,7 +281,9 @@ const AdvancedOperation = (props) => {
 
   const handleDownoadMetadataToggle = async () => {
     // setLoading(true);
-    setDownloadMetadataToggle((downloadMetadataToggle) => !downloadMetadataToggle)
+    setDownloadMetadataToggle(
+      (downloadMetadataToggle) => !downloadMetadataToggle
+    );
   };
 
   const getPublishProjectButton = async () => {
@@ -343,7 +347,7 @@ const AdvancedOperation = (props) => {
 
   const ArchiveProject = useSelector((state) => state.getArchiveProject.data);
   const [isArchived, setIsArchived] = useState(false);
-  const [downloadMetadataToggle, setDownloadMetadataToggle] = useState(true)
+  const [downloadMetadataToggle, setDownloadMetadataToggle] = useState(true);
   const getArchiveProjectAPI = () => {
     const projectObj = new GetArchiveProjectAPI(id);
     dispatch(APITransport(projectObj));
@@ -378,7 +382,7 @@ const AdvancedOperation = (props) => {
 
   const handleok = () => {
     getArchiveProjectAPI();
-    getProjectDetails()
+    getProjectDetails();
     setIsArchived(!isArchived);
     setOpen(false);
   };
@@ -395,7 +399,6 @@ const AdvancedOperation = (props) => {
   useEffect(() => {
     setLoading(apiLoading);
   }, [apiLoading]);
-
 
   const renderSnackBar = () => {
     return (
@@ -474,7 +477,11 @@ const AdvancedOperation = (props) => {
               color="error"
               onClick={handleClickOpen}
               label={isArchived ? "Archived" : "Archive"}
-              disabled ={userRole.WorkspaceManager === loggedInUserData?.role?true:false}
+              disabled={
+                userRole.WorkspaceManager === loggedInUserData?.role
+                  ? true
+                  : false
+              }
             />
           </Grid>
 
@@ -487,37 +494,38 @@ const AdvancedOperation = (props) => {
             xl={12}
             sx={{ ml: 2, height: "20px", mb: 2 }}
           >
-                        {userRole.WorkspaceManager === loggedInUserData?.role ? null :
-<FormControl size="small" className={classes.formControl}>
-              <InputLabel
-                id="Select-Task-Statuses"
-                sx={{ fontSize: "16px", padding: "3px" }}
-              >
-                Select Task Statuses
-              </InputLabel>
-              <Select
-                labelId="Select-Task-Statuses"
-                label="Select Task Statuses"
-                multiple
-                value={taskStatus}
-                onChange={handleChange}
-                renderValue={(taskStatus) => taskStatus.join(", ")}
-                MenuProps={MenuProps}
-              >
-                {FilterProgressType.map((option) => (
-                  <MenuItem
-                    sx={{ textTransform: "capitalize" }}
-                    key={option}
-                    value={option}
-                  >
-                    <ListItemIcon>
-                      <Checkbox checked={taskStatus.indexOf(option) > -1} />
-                    </ListItemIcon>
-                    <ListItemText primary={snakeToTitleCase(option)} />
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>}
+            {userRole.WorkspaceManager === loggedInUserData?.role ? null : (
+              <FormControl size="small" className={classes.formControl}>
+                <InputLabel
+                  id="Select-Task-Statuses"
+                  sx={{ fontSize: "16px", padding: "3px" }}
+                >
+                  Select Task Statuses
+                </InputLabel>
+                <Select
+                  labelId="Select-Task-Statuses"
+                  label="Select Task Statuses"
+                  multiple
+                  value={taskStatus}
+                  onChange={handleChange}
+                  renderValue={(taskStatus) => taskStatus.join(", ")}
+                  MenuProps={MenuProps}
+                >
+                  {FilterProgressType.map((option) => (
+                    <MenuItem
+                      sx={{ textTransform: "capitalize" }}
+                      key={option}
+                      value={option}
+                    >
+                      <ListItemIcon>
+                        <Checkbox checked={taskStatus.indexOf(option) > -1} />
+                      </ListItemIcon>
+                      <ListItemText primary={snakeToTitleCase(option)} />
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            )}
           </Grid>
         </Grid>
 
@@ -534,16 +542,8 @@ const AdvancedOperation = (props) => {
           sx={{ float: "left" }}
           columnSpacing={2}
         >
-          {ProjectDetails.project_type == 'ContextualTranslationEditing' ? (
-            <Grid
-              item
-              xs={12}
-              sm={12}
-              md={12}
-              lg={12}
-              xl={12}
-
-            >
+          {ProjectDetails.project_type == "ContextualTranslationEditing" ? (
+            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
               <CustomButton
                 sx={{
                   inlineSize: "max-content",
@@ -551,11 +551,14 @@ const AdvancedOperation = (props) => {
                   borderRadius: 3,
                   ml: 2,
                   width: "300px",
-
                 }}
                 onClick={handleDownloadProjectAnnotations}
-                label="Downoload Project Annotations" />
-            </Grid>) : " "}
+                label="Downoload Project Annotations"
+              />
+            </Grid>
+          ) : (
+            " "
+          )}
           {/* <div className={classes.divider} ></div> */}
           <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
             {ProjectTypes?.output_dataset?.save_type === "new_record" ? (
@@ -569,8 +572,11 @@ const AdvancedOperation = (props) => {
                 }}
                 onClick={handleOpenExportProjectDialog}
                 label="Export Project into Dataset"
-                disabled={userRole.WorkspaceManager === loggedInUserData?.role ? true : false}
-
+                disabled={
+                  userRole.WorkspaceManager === loggedInUserData?.role
+                    ? true
+                    : false
+                }
               />
             ) : (
               <CustomButton
@@ -583,14 +589,18 @@ const AdvancedOperation = (props) => {
                 }}
                 onClick={handleExportProject}
                 label="Export Project into Dataset"
-                disabled={userRole.WorkspaceManager === loggedInUserData?.role ? true : false}
-
+                disabled={
+                  userRole.WorkspaceManager === loggedInUserData?.role
+                    ? true
+                    : false
+                }
               />
             )}
           </Grid>
 
           <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-            {ProjectDetails.sampling_mode == "f" || ProjectDetails.sampling_mode == "b" ? (
+            {ProjectDetails.sampling_mode == "f" ||
+            ProjectDetails.sampling_mode == "b" ? (
               <CustomButton
                 sx={{
                   inlineSize: "max-content",
@@ -601,7 +611,11 @@ const AdvancedOperation = (props) => {
                 }}
                 onClick={handlePullNewData}
                 label="Pull New Data Items from Source Dataset"
-                disabled={userRole.WorkspaceManager === loggedInUserData?.role ? true : false}
+                disabled={
+                  userRole.WorkspaceManager === loggedInUserData?.role
+                    ? true
+                    : false
+                }
               />
             ) : (
               " "
@@ -620,7 +634,6 @@ const AdvancedOperation = (props) => {
           <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
             <DeallocationAnnotatorsAndReviewers />
           </Grid>
-
         </Grid>
 
         <Grid
@@ -646,44 +659,45 @@ const AdvancedOperation = (props) => {
               onChange={handleReviewToggle}
             />
           </Grid> */}
-          <Grid item xs={12} sm={12} md={12} lg={12} xl={12}
-            sx={{ ml: 2 }}
-          >
-              <FormControl size="small" className={classes.formControl}>
-                <InputLabel id="task-Reviews-label" sx={{ fontSize: "16px" }}>
-                  Project Stage
-                </InputLabel>
-                <Select
-                  labelId="task-Reviews-label"
-                  id="task-Reviews-select"
-                  value={taskReviews}
-                  label="Task Reviews"
-                  onChange={handleReviewToggle}
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12} sx={{ ml: 2 }}>
+            <FormControl size="small" className={classes.formControl}>
+              <InputLabel id="task-Reviews-label" sx={{ fontSize: "16px" }}>
+                Project Stage
+              </InputLabel>
+              <Select
+                labelId="task-Reviews-label"
+                id="task-Reviews-select"
+                value={taskReviews}
+                label="Task Reviews"
+                onChange={handleReviewToggle}
                 // getOptionDisabled={(option) => option.disabled}
-                >
-                  {projectStage.map((type, index) => (
-                    <MenuItem value={type.value} key={index} disabled={type.disabled} >
-                      {type.name}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
+              >
+                {projectStage.map((type, index) => (
+                  <MenuItem
+                    value={type.value}
+                    key={index}
+                    disabled={type.disabled}
+                  >
+                    {type.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
           </Grid>
 
-          {((userRole.WorkspaceManager === loggedInUserData?.role ||
-            userRole.OrganizationOwner === loggedInUserData?.role ||
-            userRole.Admin === loggedInUserData?.role) ? ProjectDetails?.project_stage == 3 : false ||
-          ProjectDetails?.review_supercheckers?.some(
-            (superchecker) => superchecker.id === loggedInUserData?.id
-          )) &&
-            <Grid item xs={12} sm={12} md={12} lg={6} xl={6}
-            >
+          {(userRole.WorkspaceManager === loggedInUserData?.role ||
+          userRole.OrganizationOwner === loggedInUserData?.role ||
+          userRole.Admin === loggedInUserData?.role
+            ? ProjectDetails?.project_stage == 3
+            : false ||
+              ProjectDetails?.review_supercheckers?.some(
+                (superchecker) => superchecker.id === loggedInUserData?.id
+              )) && (
+            <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
               <SuperCheckSettings ProjectDetails={ProjectDetails} />
-            </Grid>}
+            </Grid>
+          )}
         </Grid>
-
-
-
 
         <Grid
           container
@@ -735,13 +749,17 @@ const AdvancedOperation = (props) => {
             />
           </DialogContent>
           <DialogActions>
-            <Button onClick={handleClose}
-              variant="outlined"
-              color="error">Cancel</Button>
-            <Button onClick={handleConfirm}
+            <Button onClick={handleClose} variant="outlined" color="error">
+              Cancel
+            </Button>
+            <Button
+              onClick={handleConfirm}
               variant="contained"
               color="error"
-              autoFocus>Confirm</Button>
+              autoFocus
+            >
+              Confirm
+            </Button>
           </DialogActions>
         </Dialog>
         {OpenExportProjectDialog && (
