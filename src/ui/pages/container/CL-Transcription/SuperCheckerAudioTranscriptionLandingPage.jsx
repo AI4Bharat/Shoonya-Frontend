929879a33fa8ec58d6c62834bb354be66e38a7cc
Pursottam6003 <rahulsah6003@gmail.com>
2024-03-06 11:45:28 +0530

    updated the unused files

--- src/ui/pages/container/CL-Transcription/SuperCheckerAudioTranscriptionLandingPage.jsx
+++ src/ui/pages/container/CL-Transcription/SuperCheckerAudioTranscriptionLandingPage.jsx
@@ -112,7 +112,7 @@ const SuperCheckerAudioTranscriptionLandingPage = () => {
   const [advancedWaveformSettings, setAdvancedWaveformSettings] = useState(false);
   const [assignedUsers, setAssignedUsers] = useState(null);
   const [autoSave, setAutoSave] = useState(true);
-  const [waveSurfer, setWaveSurfer] = useState(true);
+  const [waveSurfer, setWaveSurfer] = useState(false);
   const [autoSaveTrigger, setAutoSaveTrigger] = useState(false);
 
   // useEffect(() => {
