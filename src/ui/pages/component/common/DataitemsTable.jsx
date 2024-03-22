90ae0c58714441be39174412406e45801824a8a9
kartikvirendrar <kartikrajput357@gmail.com>
2024-03-22 16:07:58 +0530

    added input fields for language, ocr domain and prediction

--- src/ui/pages/component/common/DataitemsTable.jsx
+++ src/ui/pages/component/common/DataitemsTable.jsx
@@ -33,7 +33,7 @@ const excludeKeys = [
   "unverified_conversation_json",
   "annotation_bboxes",
   "annotation_labels",
-  "annotation_transcripts",
+  "annotation_transcripts"
 ];
 
 const DataitemsTable = () => {
@@ -102,7 +102,7 @@ useEffect(() => {
               align: "center",
               customHeadLabelRender: customColumnHead,
               customBodyRender: (value) => {
-                if ((key == "metadata_json" || key == "prediction_json"|| key == "ocr_prediction_json"|| key == "transcribed_json"|| key == "draft_data_json" || key == "ocr_transcribed_json") && value !== null ) {
+                if ((key == "metadata_json" || key == "prediction_json"|| key == "ocr_prediction_json"|| key == "transcribed_json"|| key == "draft_data_json" || key == "ocr_transcribed_json" || key == "annotated_document_details_json") && value !== null ) {
                  const data = JSON.stringify(value)
                  const metadata = data.replace(/\\/g, "");
                   return metadata;
