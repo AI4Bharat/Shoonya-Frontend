dca4202d71dafdc243a89704aa62f70fb0e33199
kartikvirendrar <kartikrajput357@gmail.com>
2024-05-02 14:00:10 +0530

    changed audio urls for ANTE type

--- src/ui/pages/container/CL-Transcription/AudioTranscriptionLandingPage.jsx
+++ src/ui/pages/container/CL-Transcription/AudioTranscriptionLandingPage.jsx
@@ -112,6 +112,7 @@ const AudioTranscriptionLandingPage = () => {
   const [autoSave, setAutoSave] = useState(true);
   const [waveSurfer, setWaveSurfer] = useState(true);
   const [autoSaveTrigger, setAutoSaveTrigger] = useState(false);
+  const [audioURL, setAudioURL] = useState("");
 
   // useEffect(() => {
   //   let intervalId;
@@ -292,6 +293,27 @@ const AudioTranscriptionLandingPage = () => {
         setWaveSurfer(false);
       }else{
         setWaveSurfer(true);
+      }
+      const fetchAudioData = await fetch(String(resp?.data?.audio_url).replace("https://asr-transcription.objectstore.e2enetworks.net/", `https://${configs.BASE_URL_AUTO}/task/get_audio_file/?audio_url=`), {
+        method: "GET",
+        headers: ProjectObj.getHeaders().headers
+      })
+      if (!fetchAudioData.ok){
+        setAudioURL(resp?.data?.audio_url)
+      }else{
+        try {
+          var base64data = await fetchAudioData.json();
+          var binaryData = atob(base64data);
+          var buffer = new ArrayBuffer(binaryData.length);
+          var view = new Uint8Array(buffer);
+          for (var i = 0; i < binaryData.length; i++) {
+              view[i] = binaryData.charCodeAt(i);
+          }
+          var blob = new Blob([view], { type: 'audio/mpeg' });
+          setAudioURL(URL.createObjectURL(blob));
+        } catch {
+          setAudioURL(resp?.data?.audio_url)
+        }
       }
     }
     setLoading(false);
@@ -927,6 +949,7 @@ useEffect(() => {
               filterMessage={filterMessage}
               taskData={taskData}
             />
+            {audioURL && 
             <AudioPanel
               setCurrentTime={setCurrentTime}
               setPlaying={setPlaying}
@@ -934,7 +957,8 @@ useEffect(() => {
               onNextAnnotation={onNextAnnotation}
               AnnotationsTaskDetails={AnnotationsTaskDetails}
               taskData={taskData}
-            />
+              audioUrl={audioURL}
+            />}
             <Grid container spacing={1} sx={{ pt: 1, pl: 2, pr : 3}} justifyContent="flex-end">
              <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center" justifyContent="flex-end" width="fit-content">
                 <Typography fontSize={14} fontWeight={"medium"} color="#555">
@@ -1210,7 +1234,7 @@ useEffect(() => {
         bottom={1}
       // style={fullscreen ? { visibility: "hidden" } : {}}
       >
-        {waveSurfer ? <Timeline2 key={taskDetails?.data?.audio_url} details={taskDetails} waveformSettings={waveSurferWaveformSettings}/> : <Timeline currentTime={currentTime} playing={playing} taskID={taskData?.id} waveformSettings={waveformSettings}/>} 
+        {audioURL && (waveSurfer ? <Timeline2 key={taskDetails?.data?.audio_url} details={taskDetails} waveformSettings={waveSurferWaveformSettings}/> : <Timeline currentTime={currentTime} playing={playing} taskID={taskData?.id} waveformSettings={waveformSettings}/>)} 
       </Grid>
     </>
   );
