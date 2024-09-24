209b4596917bb9a7c90ea9296fd07d0003e70c56
Rahul-538 <krrahul803213@gmail.com>
2024-09-24 16:09:45 +0530

    "Added metadata in Read-only tab"

--- src/redux/actions/api/ProjectDetails/GetProjectDetails.js
+++ src/redux/actions/api/ProjectDetails/GetProjectDetails.js
@@ -1,42 +1,66 @@
 /**
  * Login API
  */
- import API from "../../../api";
- import ENDPOINTS from "../../../../config/apiendpoint";
+import API from "../../../api";
+import ENDPOINTS from "../../../../config/apiendpoint";
 import constants from "../../../constants";
- 
- export default class GetProjectDetailsAPI extends API {
-   constructor(projectId, timeout = 2000) {
-     super("GET", timeout, false);
-     this.type = constants.GET_PROJECT_DETAILS;
-     this.endpoint = `${super.apiEndPointAuto()}${ENDPOINTS.getProjects}${projectId}`;
-   }
- 
-   processResponse(res) {
-     super.processResponse(res);
-     if (res) {
-         this.projectDetails = res;
-     }
- }
- 
-   apiEndPoint() {
-     return this.endpoint;
-   }
- 
-   getBody() {}
- 
-   getHeaders() {
-     this.headers = {
-       headers: {
-         "Content-Type": "application/json",
-         "Authorization":`JWT ${localStorage.getItem('shoonya_access_token')}`
-       },
-     };
-     return this.headers;
-   }
- 
-   getPayload() {
-     return this.projectDetails
-   }
- }
- 
+
+export default class GetProjectDetailsAPI extends API {
+  constructor(projectId, method = 'GET', payload = null, timeout = 2000) {
+    super(method, timeout, false);
+    this.projectId = projectId;
+    this.method = method.toUpperCase();
+    this.payload = payload;
+
+    if (this.method === 'GET') {
+      this.type = constants.GET_PROJECT_DETAILS;
+    } else if (this.method === 'PATCH') {
+      this.type = constants.PATCH_PROJECT_DETAILS;
+    } else {
+      throw new Error(`Unsupported HTTP method: ${method}`);
+    }
+    
+    this.endpoint = `${super.apiEndPointAuto()}${ENDPOINTS.getProjects}${projectId}${this.method === 'PATCH' ? '/' : ''}`;
+  }
+
+  processResponse(res) {
+    super.processResponse(res);
+    if (res) {
+      if (this.method === 'GET') {
+        this.projectDetails = res;
+      } else if (this.method === 'PATCH') {
+        this.updatedProjectDetails = res;
+      }
+    }
+  }
+
+  apiEndPoint() {
+    return this.endpoint;
+  }
+
+  getBody() {
+    if (this.method === 'PATCH' && this.payload) {
+      return JSON.stringify(this.payload);
+    }
+    return null;
+  }
+
+  getHeaders() {
+    this.headers = {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `JWT ${localStorage.getItem('shoonya_access_token')}`,
+      },
+    };
+    return this.headers;
+  }
+
+  getPayload() {
+    if (this.method === 'GET') {
+      return this.projectDetails;
+    } else if (this.method === 'PATCH') {
+      return this.updatedProjectDetails;
+    }
+    return null;
+  }
+}
