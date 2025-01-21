232c200fc7aa22829145208a6a311ec6908469fa
munishmangla98 <munishmangla98@gmail.com>
2025-01-21 15:01:30 +0530

    updated code loader applied dataset

--- src/ui/pages/container/Dataset/DatasetList.jsx
+++ src/ui/pages/container/Dataset/DatasetList.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from "react";
 import { Radio, Box, Grid, Typography, ThemeProvider } from "@mui/material";
 import RadioGroup from "@mui/material/RadioGroup";
@@ -21,23 +20,22 @@ export default function DatasetList() {
   const dispatch = useDispatch();
   const classes = DatasetStyle();
   const navigate = useNavigate();
-
   const [radiobutton, setRadiobutton] = useState(true);
-  const [loading, setLoading] = useState(true); // Ensure spinner shows initially
+  const [loading, setLoading] = useState(true);
+
   const datasetList = useSelector((state) => state.getDatasetList.data);
   const apiLoading = useSelector((state) => state.apiStatus.loading);
+  const loggedInUserData = useSelector(
+    (state) => state.fetchLoggedInUserData.data
+  );
 
   const [selectedFilters, setsSelectedFilters] = useState({
     dataset_visibility: "",
     dataset_type: "",
   });
 
-  const loggedInUserData = useSelector(
-    (state) => state.fetchLoggedInUserData.data
-  );
-
   const getDashboardprojectData = () => {
-    setLoading(true); // Ensure loading is true when fetching data
+    setLoading(true);
     const projectObj = new GetDatasetsAPI(selectedFilters);
     dispatch(APITransport(projectObj));
   };
@@ -47,8 +45,7 @@ export default function DatasetList() {
   }, [selectedFilters]);
 
   useEffect(() => {
-    if (!apiLoading && datasetList?.length >= 0) {
-      // Stop loading when the API finishes and data is fetched
+    if (!apiLoading && datasetList) {
       setLoading(false);
     }
   }, [apiLoading, datasetList]);
@@ -56,100 +53,98 @@ export default function DatasetList() {
   const handleProjectlist = () => {
     setRadiobutton(true);
   };
+
   const handleProjectcard = () => {
     setRadiobutton(false);
   };
-  const handleCreateProject = (e) => {
+
+  const handleCreateProject = () => {
     navigate(`/create-Dataset-Instance-Button/`);
   };
 
-  const handleAutomateButton = (e) => {
+  const handleAutomateButton = () => {
     navigate("/datasets/automate");
   };
 
   return (
     <ThemeProvider theme={themeDefault}>
-      {loading ? (
-        <Spinner /> // Spinner shown when loading is true
-      ) : (
-        <>
-          <Grid container className={classes.root}>
-            <Grid item style={{ flexGrow: "0" }}>
-              <Typography variant="h6" sx={{ paddingBottom: "8px" }}>
-                View :{" "}
-              </Typography>
-            </Grid>
-            <Grid item style={{ flexGrow: "1", paddingLeft: "5px" }}>
-              <FormControl>
-                <RadioGroup
-                  row
-                  aria-labelledby="demo-row-radio-buttons-group-label"
-                  name="row-radio-buttons-group"
-                  defaultValue="DatasetList"
-                >
-                  <FormControlLabel
-                    value="DatasetList"
-                    control={<Radio />}
-                    label="List"
-                    onClick={handleProjectlist}
-                  />
-                  <FormControlLabel
-                    value="DatasetCard"
-                    control={<Radio />}
-                    label="Card"
-                    onClick={handleProjectcard}
-                  />
-                </RadioGroup>
-              </FormControl>
-            </Grid>
-            <Grid xs={3} item className={classes.fixedWidthContainer}>
-              <Search />
-            </Grid>
-          </Grid>
+      {loading && <Spinner />}
 
-          <Box>
-            <CustomButton
-              sx={{
-                p: 2,
-                borderRadius: 3,
-                mt: 2,
-                mb: 2,
-                justifyContent: "flex-end",
-              }}
-              onClick={handleCreateProject}
-              label="Create New Dataset Instance"
+      <Grid container className={classes.root}>
+        <Grid item style={{ flexGrow: "0" }}>
+          <Typography variant="h6" sx={{ paddingBottom: "8px" }}>
+            View :{" "}
+          </Typography>
+        </Grid>
+        <Grid item style={{ flexGrow: "1", paddingLeft: "5px" }}>
+          <FormControl>
+            <RadioGroup
+              row
+              aria-labelledby="demo-row-radio-buttons-group-label"
+              name="row-radio-buttons-group"
+              defaultValue="DatasetList"
+            >
+              <FormControlLabel
+                value="DatasetList"
+                control={<Radio />}
+                label="List"
+                onClick={handleProjectlist}
+              />
+              <FormControlLabel
+                value="DatasetCard"
+                control={<Radio />}
+                label="Card"
+                onClick={handleProjectcard}
+              />
+            </RadioGroup>
+          </FormControl>
+        </Grid>
+        <Grid xs={3} item className={classes.fixedWidthContainer}>
+          <Search />
+        </Grid>
+      </Grid>
+
+      <Box>
+        <CustomButton
+          sx={{
+            p: 2,
+            borderRadius: 3,
+            mt: 2,
+            mb: 2,
+            justifyContent: "flex-end",
+          }}
+          onClick={handleCreateProject}
+          label="Create New Dataset Instance"
+        />
+        <CustomButton
+          sx={{
+            p: 2,
+            borderRadius: 3,
+            mt: 2,
+            mb: 2,
+            ml: 2,
+            justifyContent: "flex-end",
+          }}
+          disabled={userRole.Admin === loggedInUserData?.role ? false : true}
+          onClick={handleAutomateButton}
+          label="Automate Datasets"
+        />
+        <Box sx={{ p: 1 }}>
+          {radiobutton ? (
+            <DatasetCardList
+              datasetList={datasetList}
+              selectedFilters={selectedFilters}
+              setsSelectedFilters={setsSelectedFilters}
             />
-            <CustomButton
-              sx={{
-                p: 2,
-                borderRadius: 3,
-                mt: 2,
-                mb: 2,
-                ml: 2,
-                justifyContent: "flex-end",
-              }}
-              disabled={userRole.Admin === loggedInUserData?.role ? false : true}
-              onClick={handleAutomateButton}
-              label="Automate Datasets"
+          ) : (
+            <DatasetCard
+              datasetList={datasetList}
+              selectedFilters={selectedFilters}
+              setsSelectedFilters={setsSelectedFilters}
             />
-            <Box sx={{ p: 1 }}>
-              {radiobutton ? (
-                <DatasetCardList
-                  datasetList={datasetList}
-                  selectedFilters={selectedFilters}
-                  setsSelectedFilters={setsSelectedFilters}
-                />
-              ) : (
-                <DatasetCard
-                  datasetList={datasetList}
-                  selectedFilters={selectedFilters}
-                  setsSelectedFilters={setsSelectedFilters}
-                />
-              )}
-            </Box>
-          </Box>
-        </>
-      )}
+          )}
+        </Box>
+      </Box>
     </ThemeProvider>
   );
 }
