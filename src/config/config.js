0d5afa605d79dbf0cdabf8189890340e48f06c10
munishmangla98 <munishmangla98@gmail.com>
2025-01-28 10:26:43 +0530

    Revert "loading bug2 clean"
    This reverts commit 273b3cb4e8cad3ee4998bb4e515338cb4dd9248f.

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ const configs = {
   BASE_URL: "https://backend.prod2.shoonya.ai4bharat.org",
   BASE_URL_AUTO: process.env.REACT_APP_APIGW_BASE_URL
     ? process.env.REACT_APP_APIGW_BASE_URL
-        :"http://127.0.0.1:8000"
+        :"https://backend.shoonya.ai4bharat.org"
 };
 
 export default configs;
@@ -10,4 +10,4 @@ export default configs;
 //https://backend.shoonya.ai4bharat.org/
 // https://backend.dev2.shoonya.ai4bharat.org
 //https://backend.prod2.shoonya.ai4bharat.org
-// http://127.0.0.1:8000
+// 
