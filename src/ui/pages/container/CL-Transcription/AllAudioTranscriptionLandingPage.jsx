9766206985b61d74488862e489f37bcd69d590b9
sagarika-padmanaban <82715252+sagarika-padmanaban@users.noreply.github.com>
2025-03-24 14:21:55 +0530

    Revert "arrow"

--- src/ui/pages/container/CL-Transcription/AllAudioTranscriptionLandingPage.jsx
+++ src/ui/pages/container/CL-Transcription/AllAudioTranscriptionLandingPage.jsx
@@ -524,13 +524,6 @@ const AllAudioTranscriptionLandingPage = () => {
           }
         }
       }
-      const activeElement = document.activeElement;
-      const isTextAreaFocused = activeElement.tagName =='TEXTAREA';
-      console.log(activeElement);
-  
-      if (isTextAreaFocused) {
-        return;
-      }
       if (event.shiftKey && event.key === 'ArrowLeft') {
         event.preventDefault();
         if (player) {
