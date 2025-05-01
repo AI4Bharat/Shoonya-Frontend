d6f3b77e1d071d8bb8e7ba0f88883faec1222bbf
Muhammad Umaid Rather <107248121+humiiii@users.noreply.github.com>
2025-05-01 15:31:08 +0530

    Fix doublehash tlp shortcut

--- src/ui/pages/container/CL-Transcription/SuperCheckerAudioTranscriptionLandingPage.jsx
+++ src/ui/pages/container/CL-Transcription/SuperCheckerAudioTranscriptionLandingPage.jsx
@@ -1027,13 +1027,7 @@ useEffect(() => {
         }
       }
     }
-    const activeElement = document.activeElement;
-    const isTextAreaFocused = activeElement.tagName =='TEXTAREA';
-    console.log(activeElement);
-
-    if (isTextAreaFocused) {
-      return;
-    }
+    
     if (event.shiftKey && event.key === 'ArrowLeft') {
       event.preventDefault();
       if(player){
