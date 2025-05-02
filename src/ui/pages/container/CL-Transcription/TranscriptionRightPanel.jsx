379280d77f9532eb2f9144b204e3478c04653ba4
Muhammad Umaid Rather <107248121+humiiii@users.noreply.github.com>
2025-05-02 17:55:23 +0530

    fixed undo in doublehash and textarea also tlp shortcuts

--- src/ui/pages/container/CL-Transcription/TranscriptionRightPanel.jsx
+++ src/ui/pages/container/CL-Transcription/TranscriptionRightPanel.jsx
@@ -404,6 +404,19 @@ const TranscriptionRightPanel = ({
       currentTarget,
     } = event;
 
+  const oldText = subtitles[index]?.text || "";
+  if (oldText !== value) {
+    setUndoStack((prevState) => [
+      ...prevState,
+      {
+        type: "textChange",
+        index: index,
+        previousText: oldText,
+      },
+    ]);
+    setRedoStack([]);
+  }
+
     const containsTripleDollar = value.includes("$$$");
 
     // setEnableTransliterationSuggestion(true);
@@ -551,6 +564,16 @@ const TranscriptionRightPanel = ({
       const doubleHashedText = `##${selectedText}##`;
 
       replaceSelectedText(doubleHashedText, currentIndexToSplitTextBlock, index);
+      setUndoStack((prevState) => [
+        ...prevState,
+        {
+          type: "doubleHash",
+          index: i,
+          originalText: selectedText,
+          hashedText: doubleHashedText,
+        },
+      ]);
+      setRedoStack([]);
     }
   }
 
@@ -576,39 +599,146 @@ const TranscriptionRightPanel = ({
     [limit, currentOffset]
   );
 
-  const onUndo = useCallback(() => {
-    if (undoStack?.length > 0) {
-      //getting last last action performed by user
-      const lastAction = undoStack[undoStack?.length - 1];
+const onUndo = useCallback(() => {
+  if (undoStack?.length > 0) {
+    const lastAction = undoStack[undoStack.length - 1];
 
-      // modifing subtitles based on last action
-      const sub = onUndoAction(lastAction);
+    if (lastAction.type === "textChange") {
+      const currentText = subtitles[lastAction.index]?.text || "";
+
+      // Undo text change
+      const sub = onSubtitleChange(
+        lastAction.previousText,
+        lastAction.index,
+        false
+      );
       dispatch(setSubtitles(sub, C.SUBTITLES));
 
-      //removing the last action from undo and putting in redo stack
-      setUndoStack(undoStack.slice(0, undoStack?.length - 1));
+      // Push redo version of this action
+      setRedoStack((prevState) => [
+        ...prevState,
+        {
+          type: "textChange",
+          index: lastAction.index,
+          previousText: currentText,
+        },
+      ]);
+    }
+
+    else if (lastAction.type === "doubleHash") {
+      // Undo double-hash action
+      const elementsWithBoxHighlightClass = document.getElementsByClassName(
+        classes.boxHighlight
+      );
+      const textArea = elementsWithBoxHighlightClass[lastAction.index];
+      if (textArea) {
+        textArea.value = textArea.value.replace(
+          lastAction.hashedText,
+          lastAction.originalText
+        );
+
+        const sub = onSubtitleChange(
+          textArea.value,
+          currentIndexToSplitTextBlock,
+          lastAction.index
+        );
+        dispatch(setSubtitles(sub, C.SUBTITLES));
+      }
+
       setRedoStack((prevState) => [...prevState, lastAction]);
     }
 
-    // eslint-disable-next-line
-  }, [undoStack, redoStack]);
+    else {
+      // Handle other action types
+      const sub = onUndoAction(lastAction);
+      dispatch(setSubtitles(sub, C.SUBTITLES));
+      setRedoStack((prevState) => [...prevState, lastAction]);
+    }
 
-  const onRedo = useCallback(() => {
-    if (redoStack?.length > 0) {
-      //getting last last action performed by user
-      const lastAction = redoStack[redoStack?.length - 1];
+    // Always pop from undo stack
+    setUndoStack((prev) => prev.slice(0, prev.length - 1));
+  }
+}, [undoStack, subtitles, dispatch, currentIndexToSplitTextBlock, classes.boxHighlight]);
 
-      // modifing subtitles based on last action
-      const sub = onRedoAction(lastAction);
+const onRedo = useCallback(() => {
+  if (redoStack?.length > 0) {
+    const lastAction = redoStack[redoStack.length - 1];
+
+    if (lastAction.type === "textChange") {
+      const currentText = subtitles[lastAction.index]?.text || "";
+
+      // Redo text change
+      const sub = onSubtitleChange(
+        lastAction.previousText,
+        lastAction.index,
+        false
+      );
       dispatch(setSubtitles(sub, C.SUBTITLES));
 
-      //removing the last action from redo and putting in undo stack
-      setRedoStack(redoStack.slice(0, redoStack?.length - 1));
+      // Push undo version of this action
+      setUndoStack((prevState) => [
+        ...prevState,
+        {
+          type: "textChange",
+          index: lastAction.index,
+          previousText: currentText,
+        },
+      ]);
+    }
+
+    else if (lastAction.type === "doubleHash") {
+      // Redo double-hash action
+      const elementsWithBoxHighlightClass = document.getElementsByClassName(
+        classes.boxHighlight
+      );
+      const textArea = elementsWithBoxHighlightClass[lastAction.index];
+      if (textArea) {
+        textArea.value = textArea.value.replace(
+          lastAction.originalText,
+          lastAction.hashedText
+        );
+
+        const sub = onSubtitleChange(
+          textArea.value,
+          currentIndexToSplitTextBlock,
+          lastAction.index
+        );
+        dispatch(setSubtitles(sub, C.SUBTITLES));
+      }
+
       setUndoStack((prevState) => [...prevState, lastAction]);
     }
 
-    // eslint-disable-next-line
-  }, [undoStack, redoStack]);
+    else {
+      // Redo other actions
+      const sub = onRedoAction(lastAction);
+      dispatch(setSubtitles(sub, C.SUBTITLES));
+      setUndoStack((prevState) => [...prevState, lastAction]);
+    }
+
+    // Remove from redo stack
+    setRedoStack((prev) => prev.slice(0, prev.length - 1));
+  }
+}, [redoStack, subtitles, dispatch, currentIndexToSplitTextBlock, classes.boxHighlight]);
+
+
+
+  useEffect(() => {
+      const handleKeyDown = (event) => {
+        if (event.ctrlKey && event.key === "z") {
+          event.preventDefault();
+          onUndo();
+        } else if (event.ctrlKey && event.key === "y") {
+          event.preventDefault();
+          onRedo();
+        }
+      };
+
+      document.addEventListener("keydown", handleKeyDown);
+      return () => {
+        document.removeEventListener("keydown", handleKeyDown);
+      };
+    }, [onUndo, onRedo]);
 
   const targetLength = (index) => {
     if (subtitles[index]?.text?.trim() !== "")
@@ -1015,7 +1145,7 @@ const TranscriptionRightPanel = ({
                           onKeyDown={(event) => {
                             console.log(event,"log",document.activeElement);
 
-                            if ( event.shiftKey && event.key == "ArrowLeft") {
+                            if ( event.shiftKey && event.key == "<") {
                               const textArea = textRefs.current[index];
                               console.log("helo");
 
@@ -1028,7 +1158,7 @@ const TranscriptionRightPanel = ({
                                 event.preventDefault(); 
                               }
                             }
-                            else if (event.shiftKey && event.key == "ArrowRight") {
+                            else if (event.shiftKey && event.key == ">") {
                               const textArea = textRefs.current[index];
                               if (textArea) {
                                 const start = textArea.selectionStart;
@@ -1092,7 +1222,7 @@ const TranscriptionRightPanel = ({
                                   onKeyDown={(event) => {
                                     console.log(event,"log",document.activeElement);
         
-                                    if ( event.shiftKey && event.key == "ArrowLeft") {
+                                    if ( event.shiftKey && event.key == "<") {
                                       const textArea = textRefs.current[index];
                                       console.log("helo");
         
@@ -1105,7 +1235,7 @@ const TranscriptionRightPanel = ({
                                         event.preventDefault(); 
                                       }
                                     }
-                                    else if (event.shiftKey && event.key == "ArrowRight") {
+                                    else if (event.shiftKey && event.key == ">") {
                                       const textArea = textRefs.current[index];
                                       if (textArea) {
                                         const start = textArea.selectionStart;
@@ -1180,7 +1310,7 @@ const TranscriptionRightPanel = ({
                             onKeyDown={(event) => {
                               console.log(event,"log",document.activeElement);
   
-                              if ( event.shiftKey && event.key == "ArrowLeft") {
+                              if ( event.shiftKey && event.key == "<") {
                                 const textArea = textRefs.current[index];
                                 console.log("helo");
   
@@ -1193,7 +1323,7 @@ const TranscriptionRightPanel = ({
                                   event.preventDefault(); 
                                 }
                               }
-                              else if (event.shiftKey && event.key == "ArrowRight") {
+                              else if (event.shiftKey && event.key == ">") {
                                 const textArea = textRefs.current[index];
                                 if (textArea) {
                                   const start = textArea.selectionStart;
@@ -1266,7 +1396,7 @@ const TranscriptionRightPanel = ({
                             onKeyDown={(event) => {
                               console.log(event,"log",document.activeElement);
   
-                              if ( event.shiftKey && event.key == "ArrowLeft") {
+                              if ( event.shiftKey && event.key == "<") {
                                 const textArea = textRefs.current[index];
                                 console.log("helo");
   
@@ -1279,7 +1409,7 @@ const TranscriptionRightPanel = ({
                                   event.preventDefault(); 
                                 }
                               }
-                              else if (event.shiftKey && event.key == "ArrowRight") {
+                              else if (event.shiftKey && event.key == ">") {
                                 const textArea = textRefs.current[index];
                                 if (textArea) {
                                   const start = textArea.selectionStart;
@@ -1348,7 +1478,7 @@ const TranscriptionRightPanel = ({
                                     onKeyDown={(event) => {
                                       console.log(event,"log",document.activeElement);
           
-                                      if ( event.shiftKey && event.key == "ArrowLeft") {
+                                      if ( event.shiftKey && event.key == "<") {
                                         const textArea = textRefs.current[index];
                                         console.log("helo");
           
@@ -1361,7 +1491,7 @@ const TranscriptionRightPanel = ({
                                           event.preventDefault(); 
                                         }
                                       }
-                                      else if (event.shiftKey && event.key == "ArrowRight") {
+                                      else if (event.shiftKey && event.key == ">") {
                                         const textArea = textRefs.current[index];
                                         if (textArea) {
                                           const start = textArea.selectionStart;
@@ -1431,7 +1561,7 @@ const TranscriptionRightPanel = ({
                               onKeyDown={(event) => {
                                 console.log(event,"log",document.activeElement);
     
-                                if ( event.shiftKey && event.key == "ArrowLeft") {
+                                if ( event.shiftKey && event.key == "<") {
                                   const textArea = textRefs.current[index];
                                   console.log("helo");
     
@@ -1444,7 +1574,7 @@ const TranscriptionRightPanel = ({
                                     event.preventDefault(); 
                                   }
                                 }
-                                else if (event.shiftKey && event.key == "ArrowRight") {
+                                else if (event.shiftKey && event.key == ">") {
                                   const textArea = textRefs.current[index];
                                   if (textArea) {
                                     const start = textArea.selectionStart;
