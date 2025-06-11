ac0bcac89206500fd174add74cd02af9402a0856
sagarika-padmanaban <sagarikapd@gmail.com>
2025-06-11 16:02:34 +0530

    fix

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const configs = {
-  BASE_URL: "https://backend.shoonya.ai4bharat.org",
+  BASE_URL: "https://backend.prod2.shoonya.ai4bharat.org",
   BASE_URL_AUTO: process.env.REACT_APP_APIGW_BASE_URL
     ? process.env.REACT_APP_APIGW_BASE_URL
         :"https://backend.shoonya.ai4bharat.org"
