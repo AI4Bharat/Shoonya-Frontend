bf6c3e5fbe5276a4f2ba2b4ab05da313595c5bdd
kartikvirendrar <kartikrajput357@gmail.com>
2025-07-28 10:17:20 +0530

    fix for sarvam e2e audio url's

--- src/ui/pages/container/CL-Transcription/AudioTranscriptionLandingPage.jsx
+++ src/ui/pages/container/CL-Transcription/AudioTranscriptionLandingPage.jsx
@@ -393,9 +393,9 @@ const AudioTranscriptionLandingPage = () => {
         : String(resp?.data?.audio_url)),
     {
         method: "GET",
-        headers: String(resp?.data?.audio_url).includes("objectstore.e2enetworks.net")
-        ? ProjectObj.getHeaders().headers
-        : {}
+        headers: String(resp?.data?.audio_url).includes("sarvam-benchmark.objectstore.e2enetworks.net")
+        ? {}
+        : ProjectObj.getHeaders().headers
       })
       if (!fetchAudioData.ok){
         setAudioURL(resp?.data?.audio_url)
