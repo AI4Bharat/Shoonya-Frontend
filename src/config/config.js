d75f38541db0706dc49b3f8b3af8d9b97aefcbfc
munishmangla98 <munishmangla98@gmail.com>
2025-12-17 10:16:10 +0530

    check

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ const configs = {
   BASE_URL: "https://backend.prod2.shoonya.ai4bharat.org",
   BASE_URL_AUTO: process.env.REACT_APP_APIGW_BASE_URL
     ? process.env.REACT_APP_APIGW_BASE_URL
-        :"https://backend.shoonya.ai4bharat.org"
+        :"http://127.0.0.1:8000",
 };
 
 export default configs;
