589e964324f52757b878feccb09a1389e0636ea6
sagarika-padmanaban <sagarikapd@gmail.com>
2026-01-12 21:07:17 +0530

    fix

--- src/ui/pages/container/CL-Transcription/SuperCheckerAudioTranscriptionLandingPage.jsx
+++ src/ui/pages/container/CL-Transcription/SuperCheckerAudioTranscriptionLandingPage.jsx
@@ -1303,9 +1303,9 @@ useEffect(() => {
                     <div className={classes.relative} style={{ width: "100%" }}>
                       <textarea
                         className={classes.customTextarea}
-                        dir={stdTranscriptionSettings.rtl ? "rtl" : "ltr"}
+                        dir={"ltr"}
                         rows={4}
-                        style={{ fontSize: stdTranscriptionSettings.fontSize, height: "120px" }}
+                        style={{ fontSize: stdTranscriptionSettings.fontSize, height: "120px",textAlign: stdTranscriptionSettings.rtl ? "right" : "left" }}
                         {...props}
                       />
                     </div>
@@ -1319,11 +1319,12 @@ useEffect(() => {
                       setStdTranscription(e.target.value);
                     }}
                     value={stdTranscription}
-                    dir={stdTranscriptionSettings.rtl ? "rtl" : "ltr"}
+                    dir={"ltr"}
                     className={classes.customTextarea}
                     style={{
                       fontSize: stdTranscriptionSettings.fontSize,
                       height: "120px",
+                      textAlign: stdTranscriptionSettings.rtl ? "right" : "left"
                     }}
                     rows={4}
                   />
