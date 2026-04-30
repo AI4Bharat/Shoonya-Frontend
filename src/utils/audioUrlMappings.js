4ce5c2a919c30a15453111799a603c46c465d7ad
Ishvinder Sethi <ishvindersethi22@gmail.com>
2026-04-30 08:09:52 +0530

    Revert "Revert "fix audio url's not loading""

---
+++ src/utils/audioUrlMappings.js
@@ -0,0 +1,14 @@
+export const AUDIO_URL_MAPPINGS = [
+  {
+    sourcePrefix: "https://asr-transcription.objectstore.e2enetworks.net/",
+    audioUrlParam: "asr-transcription/",
+  },
+  {
+    sourcePrefix: "https://indic-asr-public.objectstore.e2enetworks.net/",
+    audioUrlParam: "speechteam/",
+  },
+  {
+    sourcePrefix: "https://www.sosnm1.shakticloud.ai:9024/iitmnewbkt/",
+    audioUrlParam: "iitmnewbkt/",
+  },
+];
