d6419003e1c7d8a5c5f23087e852a990edd1b1ee
Muhammad Umaid Rather <muhammadumaid6@gmail.com>
2026-09-02 12:47:14 +0530

    chore: update BASE_URL and fallback API endpoint to production and development environments

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,8 @@
 const configs = {
-  BASE_URL: "http://127.0.0.1:8000",
+  BASE_URL: "https://backend.prod2.shoonya.ai4bharat.org",
   BASE_URL_AUTO: process.env.REACT_APP_APIGW_BASE_URL
     ? process.env.REACT_APP_APIGW_BASE_URL
-    : "http://127.0.0.1:8000",
+        :"https://backend.dev.shoonya.ai4bharat.org"
 };
 
 export default configs;
