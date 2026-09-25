bed83328e1ffd0b0c6d43d48a434cf9757e63569
Muhammad Umaid Rather <muhammadumaid6@gmail.com>
2026-09-25 19:36:08 +0530

    feat: add date range filter to task and workspace analytics

--- src/redux/actions/api/WorkspaceDetails/GetTaskAnalytics.js
+++ src/redux/actions/api/WorkspaceDetails/GetTaskAnalytics.js
@@ -3,13 +3,21 @@ import ENDPOINTS from "../../../../config/apiendpoint";
 import constants from "../../../constants";
  
  export default class WorkspaceTaskAnalyticsAPI extends API {
-   constructor(wsId,project_type_filter, timeout = 2000) {
+   constructor(wsId, projectTypeFilter, startDate, endDate, timeout = 2000) {
      super("GET", timeout, false);
      this.type = constants.WS_TASK_ANALYTICS;
-     project_type_filter=='AllTypes'?
-     this.endpoint = `${super.apiEndPointAuto()}${ENDPOINTS.getWorkspaces}${wsId}/cumulative_tasks_count_all/`
-     :
-     this.endpoint = `${super.apiEndPointAuto()}${ENDPOINTS.getWorkspaces}${wsId}/cumulative_tasks_count_all/?project_type_filter=${project_type_filter}`
+     const queryParams = new URLSearchParams();
+
+     if (projectTypeFilter !== "AllTypes") {
+       queryParams.set("project_type_filter", projectTypeFilter);
+     }
+     if (startDate && endDate) {
+       queryParams.set("start_date", startDate);
+       queryParams.set("end_date", endDate);
+     }
+
+     const queryString = queryParams.toString();
+     this.endpoint = `${super.apiEndPointAuto()}${ENDPOINTS.getWorkspaces}${wsId}/cumulative_tasks_count_all/${queryString ? `?${queryString}` : ""}`;
    }
  
    processResponse(res) {
@@ -34,7 +42,6 @@ import constants from "../../../constants";
     };
     return this.headers;
   }
-
  
    getPayload() {
      return this.wsTaskAnalytics;
